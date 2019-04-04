e710823f8eff54fa913ceceb027c6126cd8746a8
co9olguy <nathan@xanadu.ai>
2019-04-04 14:39:28 -0400

    renaming test_fock_gaussian_measurement and porting it to fockbackend

--- strawberryfields/backends/tfbackend/tests/test_fock_probs_gaussian_states.py
+++ strawberryfields/backends/tfbackend/tests/test_fock_probs_gaussian_states.py
@@ -11,6 +11,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+
 r"""Unit tests for probabilities of Fock outcomes of Gaussian states"""
 import numpy as np
 from scipy.special import factorial as fac
