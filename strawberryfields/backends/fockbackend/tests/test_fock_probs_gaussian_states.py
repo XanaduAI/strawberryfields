64ebe66ca4ad14e21dbf9f612a97fe08dfbc104a
Josh Izaac <josh146@gmail.com>
2019-04-05 11:39:45 -0400

    reverted travis

--- strawberryfields/backends/fockbackend/tests/test_fock_probs_gaussian_states.py
+++ strawberryfields/backends/fockbackend/tests/test_fock_probs_gaussian_states.py
@@ -20,18 +20,17 @@ NUM_REPEATS = 50
 
 
 def test_coherent_state(setup_backend, cutoff, pure, tol):
-    """Tests Fock probabilities on a coherent state."""
+    """Tests Fock probabilities on a coherent state agree with fock_prob and the state vector"""
     alpha = 1
-    nmax = cutoff - 1
 
     backend = setup_backend(1)
     backend.prepare_coherent_state(alpha, 0)
     state = backend.state()
 
     if state.is_pure:
-          bsd = state.ket()
+        bsd = state.ket()
     else:
-          bsd = state.dm()
+        bsd = state.dm()
 
     if pure:
         bsd = np.outer(bsd, bsd.conj())
@@ -40,7 +39,7 @@ def test_coherent_state(setup_backend, cutoff, pure, tol):
     gbs = np.empty((1, cutoff))
 
     for i in range(cutoff):
-      gbs[:,i] = state.fock_prob([i])
+        gbs[:, i] = state.fock_prob([i])
 
     n = np.arange(cutoff)
     exact = np.exp(-np.abs(alpha)**2)*(np.abs(alpha)**2**n)/fac(n)
@@ -50,18 +49,17 @@ def test_coherent_state(setup_backend, cutoff, pure, tol):
 
 
 def test_squeezed_state(setup_backend, cutoff, pure, tol):
-    """Tests Fock probabilities on a squeezed state."""
+    """Tests Fock probabilities on a squeezed state agree with  fock_prob and the state vector."""
     r = 1.0
-    nmax = cutoff - 1
 
     backend = setup_backend(1)
     backend.prepare_squeezed_state(r, 0, 0)
     state = backend.state()
 
     if state.is_pure:
-          bsd = state.ket()
+        bsd = state.ket()
     else:
-          bsd = state.dm()
+        bsd = state.dm()
 
     if pure:
         bsd = np.outer(bsd, bsd.conj())
@@ -70,7 +68,7 @@ def test_squeezed_state(setup_backend, cutoff, pure, tol):
     gbs = np.empty((1, cutoff))
 
     for i in range(cutoff):
-      gbs[:,i] = state.fock_prob([i])
+        gbs[:, i] = state.fock_prob([i])
 
     n = np.arange(cutoff)
     exact = np.where(n%2 == 0, (1/np.cosh(r))*(np.tanh(r)**(n))*fac(n)/(2**(n/2)*fac(n/2))**2, 0)
@@ -80,10 +78,9 @@ def test_squeezed_state(setup_backend, cutoff, pure, tol):
 
 
 def test_displaced_squeezed_state(setup_backend, cutoff, pure, tol):
-    """Tests Fock probabilities on a displaced state."""
+    """Tests Fock probabilities on a dispalced squeezed state agree with  fock_prob and the state vector."""
     r = 1.0
     alpha = 3+4*1j
-    nmax = cutoff - 1
 
     backend = setup_backend(1)
     backend.prepare_squeezed_state(r, 0, 0)
@@ -91,9 +88,9 @@ def test_displaced_squeezed_state(setup_backend, cutoff, pure, tol):
     state = backend.state()
 
     if state.is_pure:
-          bsd = state.ket()
+        bsd = state.ket()
     else:
-          bsd = state.dm()
+        bsd = state.dm()
 
     if pure:
         bsd = np.outer(bsd, bsd.conj())
@@ -102,13 +99,14 @@ def test_displaced_squeezed_state(setup_backend, cutoff, pure, tol):
     gbs = np.empty((1, cutoff))
 
     for i in range(cutoff):
-      gbs[:,i] = state.fock_prob([i])
+        gbs[:, i] = state.fock_prob([i])
 
     assert np.allclose(gbs.flatten(), bsd_diag.real.flatten(), atol=tol, rtol=0)
 
 
 def test_two_mode_squeezed(setup_backend, tol):
-    r = np.arcsinh(np.sqrt(1))
+    """Tests Fock probabilities on a two mode squeezed state agree with  fock_prob and the state vector."""
+    r = np.arcsinh(np.sqrt(1)) #pylint: disable=assignment-from-no-return
     nmax = 3
     theta = np.sqrt(0.5)
     phi = np.sqrt(0.5)
@@ -119,12 +117,12 @@ def test_two_mode_squeezed(setup_backend, tol):
     backend.prepare_squeezed_state(-r, 0, 1)
     backend.beamsplitter(theta, phi, 0, 1)
 
-    gbs = np.empty((nmax, nmax))
+    gbs = np.empty((bsize, nmax, nmax))
     state = backend.state()
 
     for i in range(nmax):
         for j in range(nmax):
-            gbs[i, j] = state.fock_prob(np.array([i, j]))
+            gbs[:, i, j] = state.fock_prob(np.array([i, j]))
 
     n = np.arange(nmax)
     exact = np.diag((1/np.cosh(r)**2) * np.tanh(r)**(2*n))
@@ -133,7 +131,9 @@ def test_two_mode_squeezed(setup_backend, tol):
 
 
 def test_two_mode_squeezed_complex_phase(setup_backend, tol):
-    r = np.arcsinh(np.sqrt(1))
+    """Tests Fock probabilities on a two mode squeezed state with complex phase
+    agree with fock_prob and the state vector."""
+    r = np.arcsinh(np.sqrt(1)) #pylint: disable=assignment-from-no-return
     nmax = 3
     theta = np.sqrt(0.5)
     phi = np.sqrt(0.5)*1j
@@ -144,12 +144,12 @@ def test_two_mode_squeezed_complex_phase(setup_backend, tol):
     backend.prepare_squeezed_state(r, 0, 1)
     backend.beamsplitter(theta, phi, 0, 1)
 
-    gbs = np.empty((nmax, nmax))
+    gbs = np.empty((bsize, nmax, nmax))
     state = backend.state()
 
     for i in range(nmax):
         for j in range(nmax):
-            gbs[i, j] = state.fock_prob(np.array([i, j]))
+            gbs[:, i, j] = state.fock_prob(np.array([i, j]))
 
     n = np.arange(nmax)
     exact = np.diag((1/np.cosh(r)**2) * np.tanh(r)**(2*n))
