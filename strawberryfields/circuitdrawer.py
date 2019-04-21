442908ab12699986d0b3c897a973b201a2a316eb
Josh Izaac <josh146@gmail.com>
2019-04-20 20:48:07 -0400

    made circuit drawer self contained

--- strawberryfields/circuitdrawer.py
+++ strawberryfields/circuitdrawer.py
@@ -84,33 +84,76 @@ Code details
 """
 import datetime
 import os
-from .qcircuit_strings import (
-    QUANTUM_WIRE,
-    PAULI_X_COMP,
-    PAULI_Z_COMP,
-    CONTROL,
-    TARGET,
-    COLUMN_SPACING,
-    ROW_SPACING,
-    DOCUMENT_END,
-    WIRE_OPERATION,
-    WIRE_TERMINATOR,
-    CIRCUIT_BODY_TERMINATOR,
-    CIRCUIT_BODY_START,
-    INIT_DOCUMENT,
-    PIPE,
-    D_COMP,
-    R_COMP,
-    P_COMP,
-    V_COMP,
-    FOURIER_COMP,
-    BS_MULTI_COMP,
-    S_MULTI_COMP,
-    K_COMP,
-    MULTIGATE,
-    GHOST,
-    S_COMP,
+
+# string constants used by :class:`~strawberryfields.circuitdrawer.Circuit`
+# to transform Strawberry Fields operators to latex code.
+
+DOCUMENT_CLASS = r"\documentclass{article}"
+QCIRCUIT_PACKAGE = r"\usepackage{qcircuit}"
+BEGIN_DOCUMENT = r"\begin{document}"
+DOCUMENT_END = r"\end{document}"
+CIRCUIT_START = r"\Qcircuit"
+COLUMN_SPACING = "@C={0}"  # spacing i.e. "1em"
+ROW_SPACING = "@R={0}"  # spacing
+UNIFORM_ROW_SPACING = "@!R"
+UNIFORM_COLUMN_SPACING = "@!C"
+UNIFORM_ELEMENT_SPACING = "@!"
+QUANTUM_WIRE = r"\qw"
+MULTI_QUANTUM_WIRE = r"\qw[{0}]"  # length vector i.e. "-1"
+VERTICAL_QUANTUM_WIRE = r"\qwx[{0}]"  # length vector
+WIRE_END = r"\qwa[{0}]"  # length vector
+CLASSICAL_WIRE = r"\cw[{0}]"  # length vector
+CLASSICAL_WIRE_END = r"\cwa[{0}]"  # length vector
+VERTICAL_CLASSICAL_WIRE = r"\cwx[{0}]"  # length vector
+LABELLED_GATE = r"\gate{{{0}}}"  # label i.e. "X"
+TARGET = r"\targ"
+SWAP = r"\qswap"
+MULTIGATE = r"\multigate{{{0}}}{{{1}}}"  # depth i.e. "2", label
+NON_ADJACENT_MULTIGATE = r"\sgate{{{0}}}{{{1}}}"  # gate i.e. "X", second wire vector i.e. "1"
+GHOST = r"\ghost{{{0}}}"  # gate
+CLASSICAL_GHOST = r"\cghost{{{0}}}"  # gate
+NO_GHOST = r"\nghost{{{0}}}"  # gate
+CONTROL = r"\ctrl{{{0}}}"  # length vector
+CONTROL_ON_ZERO = r"\ctrlo{{{0}}}"  # length vector
+CLASSICAL_CONTROL = r"\cctrl{{{0}}}"  # length vector
+CLASSICAL_CONTROL_ON_ZERO = r"\cctrlo{{{0}}}"  # length vector
+ISOLATED_CONTROL = r"\control"
+ISOLATED_CONTROL_ON_ZERO = r"\controlo"
+METER = r"\meter"
+BASIS_METER = r"\meterB{{{0}}}"  # basis i.e. \ket{\xi_\pm}
+SPLIT_BASIS_METER = r"\smeterB{{{0}}}{{{1}}}"  # basis, second wire vector
+MEASURE = r"\measuretab{{{0}}}"  # label
+MULTIMEASURE = r"\multimeasure{{{0}}}{{{1}}}"  # depth, label
+LEFT_WIRE_LABEL = r"\lstick{{{0}}}"  # label
+RIGHT_WIRE_LABEL = r"\rstick{{{0}}}"  # label
+BRA = r"\bra{{{0}}}"  # state i.e. "\psi"
+KET = r"\ket{{{0}}}"  # state
+
+HADAMARD_COMP = LABELLED_GATE.format("H")
+PAULI_X_COMP = LABELLED_GATE.format("X")
+PAULI_Y_COMP = LABELLED_GATE.format("Y")
+PAULI_Z_COMP = LABELLED_GATE.format("Z")
+D_COMP = LABELLED_GATE.format("D")
+S_COMP = LABELLED_GATE.format("S")
+R_COMP = LABELLED_GATE.format("R")
+P_COMP = LABELLED_GATE.format("P")
+V_COMP = LABELLED_GATE.format("V")
+K_COMP = LABELLED_GATE.format("K")
+FOURIER_COMP = LABELLED_GATE.format("F")
+
+BS_MULTI_COMP = "BS"
+S_MULTI_COMP = "S"
+
+WIRE_OPERATION = "& {0}"
+WIRE_TERMINATOR = r"\\" + "\n"
+CIRCUIT_BODY_TERMINATOR = "}\n"
+CIRCUIT_BODY_START = " {" + "\n"
+INIT_DOCUMENT = (
+    DOCUMENT_CLASS + "\n" + QCIRCUIT_PACKAGE + "\n" + BEGIN_DOCUMENT + "\n" + CIRCUIT_START
 )
+
+PIPE = "|"
+LINE_RETURN = "\n"
 
 
 class NotDrawableException(Exception):
@@ -180,7 +223,8 @@ class Circuit:
         }
 
     def _gate_from_operator(self, op):
-        """Infers the number of modes and callable Circuit class method that correspond with a Strawberry Fields operator object.
+        """Infers the number of modes and callable Circuit class method that correspond
+        with a Strawberry Fields operator object.
 
         Args:
             op (strawberryfields.ops.Gate): the Strawberry Fields operator object.
@@ -214,7 +258,8 @@ class Circuit:
 
         Raises:
             UnsupportedGateException: if the operator is not supported by the circuit drawer module.
-            ModeMismatchException: if the operator is interpreted as an n-mode gate but is applied to a number of modes != n.
+            ModeMismatchException: if the operator is interpreted as an n-mode gate but is
+                applied to a number of modes != n.
 
         """
         if not op.__class__.__name__ == "Command":
