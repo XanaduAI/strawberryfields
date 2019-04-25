fa54ce109a523b0e0ceb2268d46cdc0b47ed34df
Ville Bergholm <ville.bergholm@iki.fi>
2019-04-25 15:29:13 -0400

    Update strawberryfields/__init__.py
    Co-Authored-By: josh146 <josh146@gmail.com>

--- strawberryfields/__init__.py
+++ strawberryfields/__init__.py
@@ -62,7 +62,7 @@ from .io import save, load
 from .program import Program, _convert as convert
 from ._version import __version__
 
-__all__ = ["Engine", "Program", "convert", "version", "save"]
+__all__ = ["Engine", "Program", "convert", "version", "save", "load"]
 
 
 def version():
