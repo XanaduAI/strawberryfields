fix Gaussian vector of means scaling

strawberryfields/ops.py
@@ -1824,14 +1824,16 @@ class Gaussian(Preparation, Decomposition):
         self.p_disp = r[self.ns:]
 
         self.decomp = decomp
+
         if decomp:
             th, self.S = williamson(V, tol=tol)
             self.pure = np.abs(np.linalg.det(V) - (self.hbar/2)**(2*self.ns)) < tol
             self.nbar = np.diag(th)[:self.ns]/self.hbar - 0.5
             super().__init__([V, r])
         else:
-            # scale state
-            super().__init__([V*2/hbar, r/np.sqrt(2*hbar)])
+            # scale state for hbar=2 to be applied directly
+            # by the backend
+            super().__init__([V*2/hbar, r*np.sqrt(2/hbar)])
 
         # FIXME merge() probably does not work for Gaussians if r is not zero?
 

tests/integration/test_decompositions_integration.py
@@ -218,12 +218,14 @@ class TestGaussianBackendPrepareState:
         """Testing a displaced squeezed state"""
         eng, prog = setup_eng(3)
         cov = (hbar / 2) * np.diag([np.exp(-0.1)] * 3 + [np.exp(0.1)] * 3)
+        means = np.array([0, 0.1, 0.2, -0.1, 0.3, 0])
 
         with prog.context as q:
-            ops.Gaussian(cov, r=[0, 0.1, 0.2, -0.1, 0.3, 0], decomp=False, hbar=hbar) | q
+            ops.Gaussian(cov, r=means, decomp=False, hbar=hbar) | q
 
         state = eng.run(prog)
         assert np.allclose(state.cov(), cov, atol=tol)
+        assert np.allclose(state.means(), means, atol=tol)
 
     def test_thermal(self, setup_eng, hbar, tol):
         """Testing a thermal state"""
@@ -287,12 +289,14 @@ class TestGaussianBackendDecomposeState:
         """Testing decomposed displaced squeezed state"""
         eng, prog = setup_eng(3)
         cov = (hbar / 2) * np.diag([np.exp(-0.1)] * 3 + [np.exp(0.1)] * 3)
+        means = np.array([0, 0.1, 0.2, -0.1, 0.3, 0])
 
         with prog.context as q:
-            ops.Gaussian(cov, r=[0, 0.1, 0.2, -0.1, 0.3, 0], hbar=hbar) | q
+            ops.Gaussian(cov, r=means, hbar=hbar) | q
 
         state = eng.run(prog)
         assert np.allclose(state.cov(), cov, atol=tol)
+        assert np.allclose(state.means(), means, atol=tol)
         assert len(eng.run_progs[-1]) == 7
 
     def test_thermal(self, setup_eng, hbar, tol):