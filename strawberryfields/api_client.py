c45ed73074cb679d250e73c170c932f456717331
Zeid <zeid@xanadu.ai>
2019-06-20 11:25:02 -0400

    Add support for JobResult and JobCircuit

--- strawberryfields/api_client.py
+++ strawberryfields/api_client.py
@@ -370,3 +370,48 @@ class Job(Resource):
             Field("running_time"),
         )
         super().__init__()
+
+    def refresh_data(self):
+        super().refresh_data()
+        self.result = JobResult(self.id)
+        self.circuit = JobCircuit(self.id)
+
+
+class JobResult(Resource):
+    """
+    The API resource corresponding to the job result.
+    """
+
+    SUPPORTED_METHODS = ("GET", )
+    PATH = "jobs/{job_id}/result"
+
+    def __init__(self, job_id):
+        """
+        Initialize the JobResult resource with a pre-defined field.
+        """
+        self.fields = (
+            Field("result", json.loads),
+        )
+
+        self.PATH = self.PATH.format(job_id=job_id)
+        super().__init__()
+
+
+class JobCircuit(Resource):
+    """
+    The API resource corresponding to the job circuit.
+    """
+
+    SUPPORTED_METHODS = ("GET", )
+    PATH = "jobs/{job_id}/circuit"
+
+    def __init__(self, job_id):
+        """
+        Initialize the JobCircuit resource with a pre-defined field.
+        """
+        self.fields = (
+            Field("circuit"),
+        )
+
+        self.PATH = self.PATH.format(job_id=job_id)
+        super().__init__()
