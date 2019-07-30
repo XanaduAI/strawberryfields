879418a5dedec5fc8948005bdc0e3a713aee58fd
Nicolas Quesada <nicolas@xanadu.ai>
2019-07-31 02:05:29 +0800

    Adds tests

--- strawberryfields/backends/gaussianbackend/backend.py
+++ strawberryfields/backends/gaussianbackend/backend.py
@@ -222,9 +222,9 @@ class GaussianBackend(BaseGaussian):
         if shots != 1:
             if select is not None:
                 raise NotImplementedError("Gaussian backend currently does not support "
-                                          "postselection if shots != 1 for Fock measurement")
+                                          "postselection if shots != 1 for Threshold measurement")
             warnings.warn("Cannot simulate non-Gaussian states. "
-                          "Conditional state after Fock measurement has not been updated.")
+                          "Conditional state after Threshold measurement has not been updated.")
 
         mu = self.circuit.mean
         cov = self.circuit.scovmatxp()
