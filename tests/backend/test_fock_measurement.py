0125f4b08499814b7d77c35a1b33f7ea512db3db
Nicolas Quesada <nicolas@xanadu.ai>
2019-07-31 14:04:21 -0400

    Added backend and integration tests for measure fock with displacements

--- tests/backend/test_fock_measurement.py
+++ tests/backend/test_fock_measurement.py
@@ -156,3 +156,21 @@ class TestRepresentationIndependent:
 
             meas = backend.measure_fock([0, 1, 2])[0]
             assert np.all(np.array(meas) == 0)
+
+
+    def test_coherent_state_has_photons(self, setup_backend, pure):
+        """Test that a coherent state with a mean photon number of 4 and sampled NUM_REPEATS times will produce photons"""
+        backend = setup_backend(1)
+        alpha = 2.0
+        nonzero = False
+
+        for _ in range(NUM_REPEATS):
+            backend.reset(pure=pure)
+            backend.displacement(alpha, 0)
+            meas = backend.measure_fock([0])[0]
+            if meas > 0:
+                nonzero = True
+                break
+        assert nonzero
+
+
