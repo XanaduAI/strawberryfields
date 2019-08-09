dd4542854b4d80ce3e2437c3cdbbef856f1f898a
Eric <ericgb86@gmail.com>
2019-08-09 15:38:56 -0400

    bug fix

--- tests/backend/test_nongaussian_gates.py
+++ tests/backend/test_nongaussian_gates.py
@@ -41,30 +41,30 @@ class TestFockRepresentation:
             numer_state = s.ket()
         else:
             numer_state = s.dm()
-            
+
         #Create annihilation operator matrix
         ladder_vals = np.arange(1, cutoff)
         ladder_vals = np.sqrt(ladder_vals)
         a = np.zeros([cutoff, cutoff])
         np.fill_diagonal(a[:,1:], ladder_vals)
-        
+
         #Construct x matrix and it's third power
         x = (a + np.transpose(a))/np.sqrt(2)
         x3 = np.matmul(x, np.matmul(x,x))
-        
+
         gate = expm(1j * gamma * x3 / 3)
-        
+
         #state to be transformed
         ket1 = np.array([1.0 for n in range(cutoff)])
         ket1 /= np.linalg.norm(ket1)
-        ket1 = ket1.reshape(-1,1)
-        
+        ket = ket1.reshape(-1,1)
+
         ref_state = np.matmul(gate, ket)
-        
+
         assert np.allclose(numer_state, ref_state, atol=tol, rtol=0.0)
-        
-        
-    
+
+
+
     @pytest.mark.parametrize("kappa", KAPPAS)
     def test_kerr_interaction(self, setup_backend, kappa, cutoff, tol):
         """Tests if the Kerr interaction has the right effect on states in the Fock basis"""
