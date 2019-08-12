2f09df4de195acbb25f22c49840e2db584b53cfe
Nicolas Quesada <nicolas@xanadu.ai>
2019-08-12 09:55:44 -0400

    Adds test checking that the results of threshold measurement are either zero or one in all the modes

--- tests/backend/test_threshold_measurement.py
+++ tests/backend/test_threshold_measurement.py
@@ -63,3 +63,23 @@ class TestRepresentationIndependent:
 
             meas = backend.measure_threshold([0, 1, 2])[0]
             assert np.all(np.array(meas) == 0)
+
+
+    def test_binary_outcome(self, setup_backend, pure):
+        """Test that the outcomes of a threshold measurement is zero or one."""
+        num_modes = 2
+        for _ in range(NUM_REPEATS):
+            backend = setup_backend(num_modes)
+            backend.reset(pure=pure)
+
+            r = 0.5
+            backend.squeeze(r, 0)
+            backend.beamsplitter(np.sqrt(0.5), -np.sqrt(0.5), 0, 1)
+            meas_modes = [0, 1]
+            meas_results = backend.measure_threshold(meas_modes)
+
+            for i in range(num_modes):
+                assert meas_results[i] == 0 or meas_results[i] ==1
+
+
+
