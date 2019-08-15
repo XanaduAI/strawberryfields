a4c8253ceb853eaa5def0c12ca85d8757abc99fb
Ville Bergholm <ville.bergholm@iki.fi>
2019-08-15 17:49:09 +0300

    Test fixed.

--- tests/integration/test_parameters_integration.py
+++ tests/integration/test_parameters_integration.py
@@ -16,16 +16,15 @@ before passing them to the backends"""
 
 import inspect
 import itertools
-import logging
+import warnings
 
-import pytest
 import numpy as np
 
-import strawberryfields as sf
+import strawberryfields.program_utils as pu
 from strawberryfields import ops
 
 
-# Fock requires an integer parameter
+# ops.Fock requires an integer parameter
 scalar_arg_preparations = (
     ops.Coherent,
     ops.Squeezed,
@@ -38,17 +37,13 @@ testset = ops.one_args_gates + ops.two_args_gates + ops.channels + scalar_arg_pr
 
 
 def test_parameters_with_operations(batch_size, setup_eng):
-    """Test using different types of Parameters with different classes of Operations."""
+    """Test all combinations of different types of Parameters with different Operation subclasses.
 
-    # TODO: this test is very hard to understand. Should be split up,
-    # and separated into many individual tests that do one thing only.
+    This test is successful if no exceptions are raised.
 
-    # TODO: In the original test suite, this test appears to 'work' on the
-    # Gaussian backend, even though it never explicitly ignores non-Gaussian
-    # operations. It even worked on the Fock backends, even though the Fock
-    # backend doesn't support the ThermalLoss channel?!
-
-    # TODO: This test doesn't assert anything!!!!!
+    Some operation/backend combinations forbidden by the CircuitSpecs instance of the backend.
+    We catch these exceptions and convert them into warnings.
+    """
     eng, prog = setup_eng(2)
     r = prog.register
 
@@ -67,33 +62,29 @@ def test_parameters_with_operations(batch_size, setup_eng):
         # test batched input
         params.append(np.random.uniform(size=(batch_size,)))
 
-    def check(G, par, measure=False):
-        """Check an Operation/Parameters combination"""
+    def check(G, par):
+        """Check an Operation/Parameters combination."""
         # construct the op using the given tuple of Parameters as args
         G = G(*par)
         with prog.context as r:
-            if measure:
-                # fake a measurement for speed
-                r[0].val = 0.1
-                r[1].val = 0.2
+            # fake a measurement for speed
+            r[0].val = 0.1
+            r[1].val = 0.2
             if G.ns == 1:
                 G | r[0]
             else:
                 G | (r[0], r[1])
 
-        #prog.optimize()
         try:
             eng.run(prog, **kwargs)
-        except NotApplicableError as err:
-            # catch unapplicable op/backend combinations here
-            logging.debug(err)
-        except NotImplementedError as err:
-            # catch not yet implemented op/backend combinations here
-            logging.debug(err)
+        except pu.CircuitError as err:
+            # record CircuitSpecs-forbidden op/backend combinations here
+            warnings.warn(str(err))
 
+        # clear the program for reuse
         prog.locked = False
         prog.circuit = []
-
+        eng.reset()
 
     print(testset)
     for G in testset:
@@ -101,14 +92,12 @@ def test_parameters_with_operations(batch_size, setup_eng):
         sig = inspect.signature(G.__init__)
         n_args = len(sig.parameters) - 1  # number of parameters __init__ takes, minus self
         if n_args < 1:
-            logging.debug(
+            warnings.warn(
                 "Unexpected number of args ({}) for {}, check the testset.".format(n_args, G)
             )
 
         # shortcut, only test cartesian products up to two parameter types
         n_args = min(n_args, 2)
-
         # check all combinations of Parameter types
         for p in itertools.product(params, repeat=n_args):
-            check(G, p, measure=True)
-            eng.reset()
+            check(G, p)
