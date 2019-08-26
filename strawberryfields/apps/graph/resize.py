743ddac6f09c38a3326b039202bbdc00c8ca16ea
trbromley <brotho02@gmail.com>
2019-08-26 12:55:37 -0400

    Black files

--- strawberryfields/apps/graph/resize.py
+++ strawberryfields/apps/graph/resize.py
@@ -59,10 +59,7 @@ RESIZE_DEFAULTS = {"method": "greedy-density"}
 
 
 def resize_subgraphs(
-    subgraphs: Iterable,
-    graph: nx.Graph,
-    target: int,
-    resize_options: Optional[dict] = None,
+    subgraphs: Iterable, graph: nx.Graph, target: int, resize_options: Optional[dict] = None
 ) -> list:
     """Resize subgraphs to a given size.
 
@@ -115,16 +112,11 @@ def resize_subgraphs(
     if not callable(method):
         method = METHOD_DICT[method]
 
-    return method(
-        subgraphs=subgraphs, graph=graph, target=target, resize_options=resize_options
-    )
+    return method(subgraphs=subgraphs, graph=graph, target=target, resize_options=resize_options)
 
 
 def greedy_density(
-    subgraphs: Iterable,
-    graph: nx.Graph,
-    target: int,
-    resize_options: Optional[dict] = None,
+    subgraphs: Iterable, graph: nx.Graph, target: int, resize_options: Optional[dict] = None
 ) -> list:
     """Method to greedily resize subgraphs based upon density.
 
@@ -181,10 +173,7 @@ def greedy_density(
 
 
 def greedy_degree(
-    subgraphs: Iterable,
-    graph: nx.Graph,
-    target: int,
-    resize_options: Optional[dict] = None,
+    subgraphs: Iterable, graph: nx.Graph, target: int, resize_options: Optional[dict] = None
 ) -> list:
     """Method to greedily resize subgraphs based upon vertex degree.
 
