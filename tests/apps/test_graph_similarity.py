fa3f8eb56429b0746fb6396035589d7518fbebe2
Tom Bromley <49409390+trbromley@users.noreply.github.com>
2019-09-12 16:47:32 -0400

    Apps layer: sample_to_event and related functions for graph similarity (#181)
    * Add skeleton for similarity module
    
    * Add documentation rst for similarity module
    
    * Add sample_to_orbit
    
    * Add sample_to_event
    
    * Add sample_to_orbit tests
    
    * Update sample_to_orbit
    
    * Update sample_to_orbit test
    
    * Correct typo
    
    * Provide orbits function
    
    * Provide tests for sample_to_event
    
    * Add orbits to autosummary
    
    * Add examples of code use in docstrings
    
    * Add functions for generating uniform samples from a given orbit or event
    
    * Add tests for generating uniform sample from given orbit or event
    
    * black and pylint changes
    
    * Revert changes that should belong to different branch
    
    * Revert changes that should belong to separate branch
    
    * Add new test to orbits
    
    * Apply suggestions from code review
    
    * Apply suggestions from code review
    
    Co-Authored-By: Josh Izaac <josh146@gmail.com>
    
    * Apply suggestions from code review
    
    * Apply suggestions from code review
    
    * Apply suggestions from code review
    
    * Update strawberryfields/apps/graph/similarity.py
    
    Co-Authored-By: Jeremy Swinarton <jeremy@xanadu.ai>
    
    * Apply suggestions from code review
    
    * Apply suggestions from code review
    
    * Apply suggestions from code review
    
    * Apply suggestions from code review

---
+++ tests/apps/test_graph_similarity.py
@@ -0,0 +1,91 @@
+# Copyright 2019 Xanadu Quantum Technologies Inc.
+
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+
+#     http://www.apache.org/licenses/LICENSE-2.0
+
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+r"""
+Unit tests for strawberryfields.apps.graph.similarity
+"""
+# pylint: disable=no-self-use,unused-argument,too-many-arguments
+import itertools
+
+import pytest
+
+from strawberryfields.apps.graph import similarity
+
+pytestmark = pytest.mark.apps
+
+all_orbits = {
+    3: [[1, 1, 1], [2, 1], [3]],
+    4: [[1, 1, 1, 1], [2, 1, 1], [3, 1], [2, 2], [4]],
+    5: [[1, 1, 1, 1, 1], [2, 1, 1, 1], [3, 1, 1], [2, 2, 1], [4, 1], [3, 2], [5]],
+}
+
+all_events = {
+    (3, 1): [3, None, None],
+    (4, 1): [4, None, None, None, None],
+    (5, 1): [5, None, None, None, None, None, None],
+    (3, 2): [3, 3, None],
+    (4, 2): [4, 4, None, 4, None],
+    (5, 2): [5, 5, None, 5, None, None, None],
+}
+
+
+@pytest.mark.parametrize("dim", [3, 4, 5])
+def test_sample_to_orbit(dim):
+    """Test if function ``similarity.sample_to_orbit`` correctly returns the original orbit after
+    taking all permutations over the orbit. The starting orbits are all orbits for a fixed photon
+    number ``dim``."""
+    orbits = all_orbits[dim]
+    checks = []
+    for o in orbits:
+        sorted_sample = o.copy()
+        sorted_sample_len = len(sorted_sample)
+        if sorted_sample_len != dim:
+            sorted_sample += [0] * sorted_sample_len
+        permutations = itertools.permutations(sorted_sample)
+        checks.append(all([similarity.sample_to_orbit(p) == o for p in permutations]))
+    assert all(checks)
+
+
+@pytest.mark.parametrize("dim", [3, 4, 5])
+class TestOrbits:
+    """Tests for the function ``strawberryfields.apps.graph.similarity.orbits``"""
+
+    def test_orbit_sum(self, dim):
+        """Test if function generates orbits that are lists that sum to ``dim``."""
+        assert all([sum(o) == dim for o in similarity.orbits(dim)])
+
+    def test_orbit_sorted(self, dim):
+        """Test if function generates orbits that are lists sorted in descending order."""
+        assert all([o == sorted(o, reverse=True) for o in similarity.orbits(dim)])
+
+    def test_orbits(self, dim):
+        """Test if function returns all the integer partitions of 5. This test does not
+        require ``similarity.orbits`` to return the orbits in any specified order."""
+        partition = all_orbits[dim]
+        orbits = similarity.orbits(dim)
+
+        assert sorted(partition) == sorted(orbits)
+
+
+@pytest.mark.parametrize("dim", [3, 4, 5])
+@pytest.mark.parametrize("max_count_per_mode", [1, 2])
+def test_sample_to_event(dim, max_count_per_mode):
+    """Test if function ``similarity.sample_to_event`` gives the correct set of events when
+    applied to all orbits with a fixed number of photons ``dim``. This test ensures that orbits
+    exceeding the ``max_count_per_mode`` value are attributed the ``None`` event and that orbits
+    not exceeding the ``max_count_per_mode`` are attributed the event ``dim``."""
+    orbits = all_orbits[dim]
+    target_events = all_events[(dim, max_count_per_mode)]
+    events = [similarity.sample_to_event(o, max_count_per_mode) for o in orbits]
+
+    assert events == target_events
