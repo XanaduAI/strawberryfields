1219f7d08e25fd8bc4678fa693475403b2cae0bd
trbromley <brotho02@gmail.com>
2019-09-12 11:41:15 -0400

    Apply suggestions from code review

--- tests/apps/test_graph_similarity.py
+++ tests/apps/test_graph_similarity.py
@@ -69,15 +69,21 @@ class TestOrbits:
         assert not partition
 
 
+all_orbits = {
+    3: [[1, 1, 1], [2, 1], [3]],
+    4: [[1, 1, 1, 1], [2, 1, 1], [3, 1], [2, 2], [4]],
+    5: [[1, 1, 1, 1, 1], [2, 1, 1, 1], [3, 1, 1], [2, 2, 1], [4, 1], [3, 2], [5]],
+}
+
+
 @pytest.mark.parametrize("dim", [3, 4, 5])
 @pytest.mark.parametrize("max_count_per_mode", [1, 2])
 def test_sample_to_event(dim, max_count_per_mode):
     """Test if function ``similarity.sample_to_event`` gives the correct set of events when
-    applied to all orbits with a fixed number of photons ``dim``. This test ensures: (i) that the
-    only events returned are either ``dim`` or ``None``; (ii) that orbits exceeding the
-    ``max_count_per_mode`` value are attributed the ``None`` event; and that (iii) that orbits
+    applied to all orbits with a fixed number of photons ``dim``. This test ensures that orbits
+    exceeding the ``max_count_per_mode`` value are attributed the ``None`` event and that orbits
     not exceeding the ``max_count_per_mode`` are attributed the event ``dim``."""
-    orbits = list(similarity.orbits(dim))
+    orbits = all_orbits[dim]
     events = [similarity.sample_to_event(o, max_count_per_mode) for o in orbits]
 
     max_count_correct = True
@@ -91,6 +97,5 @@ def test_sample_to_event(dim, max_count_per_mode):
             if e != dim:
                 photon_number_correct = False
 
-    assert set(events) == {dim, None}
     assert max_count_correct
     assert photon_number_correct
