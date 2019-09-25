92a6041af29620e6f593ad9cdcbdf01b1f4083e9
trbromley <brotho02@gmail.com>
2019-09-24 23:24:42 -0400

    Add skeleton of class

---
+++ strawberryfields/gbs/data.py
@@ -0,0 +1,42 @@
+# Copyright 2019 Xanadu Quantum Technologies Inc.
+
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+
+#     http://www.apache.org/licenses/LICENSE-2.0
+
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""
+GBS Datasets
+============
+"""
+import pkg_resources
+import scipy.sparse
+
+DATA_PATH = pkg_resources.resource_filename("strawberryfields", "gbs/data")
+
+
+class SampleLoader:
+    """To do"""
+
+    dat = scipy.sparse.spmatrix()
+    n_samples = 0
+
+    def __iter__(self):
+        self.count = 0
+        return self
+
+    def __next__(self):
+        if self.count < self.n_samples:
+            s = list(self.dat[self.count].toarray()[0])
+            self.count += 1
+            return s
+        raise StopIteration
+
+    def __getitem__(self, i):
+        return list(self.dat[i].toarray()[0])
