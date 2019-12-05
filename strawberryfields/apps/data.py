b037ae481053cf6a5b5f146e651b1b6dd05f01fb
trbromley <brotho02@gmail.com>
2019-12-05 12:43:01 -0500

    Rename GBS Datasets to Datasets

--- strawberryfields/apps/data.py
+++ strawberryfields/apps/data.py
@@ -12,8 +12,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 r"""
-GBS datasets
-============
+Datasets
+========
 
 **Module name:** :mod:`strawberryfields.apps.data`
 
