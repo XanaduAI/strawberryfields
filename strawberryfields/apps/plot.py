c48db32fd3cfea4e4a14e372db3d1827b8b5b1ec
trbromley <brotho02@gmail.com>
2019-12-05 12:43:40 -0500

    Rename Plotting and Visualization to just Plotting

--- strawberryfields/apps/plot.py
+++ strawberryfields/apps/plot.py
@@ -12,8 +12,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 r"""
-Plotting and visualization
-==========================
+Plotting
+========
 
 **Module name:** :mod:`strawberryfields.apps.plot`
 
