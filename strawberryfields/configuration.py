f7d1e56ccd602aa7b53cad203c29d9a14e807da9
antalszava <antalszava@gmail.com>
2020-02-27 12:33:26 -0500

    Update strawberryfields/configuration.py
    Co-Authored-By: Josh Izaac <josh146@gmail.com>

--- strawberryfields/configuration.py
+++ strawberryfields/configuration.py
@@ -224,8 +224,20 @@ def store_account(authentication_token, filename="config.toml", location="user_c
     """Stores an account in a configuration file.
 
     The configuration file can be created in the following locations:
-       - user configuration directory ("user_config")
-       - current working directory ("local")
+     
+     - A global user configuration directory ("user_config")
+     - The current working directory ("local")
+     
+     This global user configuration directory differs depending on the operating system:
+     
+     * On Linux: ``~/.config/strawberryfields``
+     * On Windows: ``~C:\Users\USERNAME\AppData\Local\Xanadu\strawberryfields``
+     * On MacOS: ``~/Library/Application Support/strawberryfields``
+     
+     By default, Strawberry Fields will load the configuration and account credentials from the global
+     user configuration directory, no matter the working directory. However, if there exists a configuration
+     file in the *local* working directory, this takes precedence. The ``"local"`` option is therefore useful
+     for maintaining per-project configuration settings.
 
     Args:
         authentication_token (str): the authentication token to use when
