630180ee7a650348607aa8d31c039957fdd84e66
Nathan Killoran <co9olguy@users.noreply.github.com>
2020-03-30 18:01:48 -0400

    Revert "fixing assignment bug (#341)"
    This reverts commit beb7500622712186a9bf7f27fd3d8f354ac17d35.

--- strawberryfields/engine.py
+++ strawberryfields/engine.py
@@ -577,7 +577,7 @@ class RemoteEngine:
         # TODO: this should be provided by the chip API, rather
         # than built-in to Strawberry Fields.
         compile_options = compile_options or {}
-        kwargs.update(self._backend_options)
+        kwargs = kwargs.update(self._backend_options)
 
         if program.target is None or (program.target.split("_")[0] != self.target.split("_")[0]):
             # Program is either:
