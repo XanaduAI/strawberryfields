44f735139b8231b5e24593f5d29edb28ea02ff56
Nicolas Quesada <zeitus@gmail.com>
2020-04-08 16:48:26 -0400

    adds Xn

---
+++ strawberryfields/circuitspecs/Xn.py
@@ -0,0 +1,64 @@
+from thewalrus.quantum import Amat
+from strawberryfields.decompositions import takagi
+from .circuit_specs import CircuitSpecs
+from strawberryfields.program_utils import CircuitError, Command, group_operations
+import strawberryfields.ops as ops
+from .gbs import GBSSpecs
+from .gaussian_unitary import GaussianUnitary
+
+
+class XnSpecs(CircuitSpecs):
+    """Circuit specifications for the X8 class of circuits."""
+
+    short_name = "Xn"
+    modes = 8
+    remote = True
+    local = True
+    interactive = False
+    sq_amplitude = 1.0
+
+    primitives = {"S2gate", "Sgate", "MeasureFock", "Rgate", "BSgate", "MZgate"}
+
+    decompositions = {
+        "Interferometer": {"mesh": "rectangular_symmetric", "drop_identity": False},
+        "BipartiteGraphEmbed": {"mesh": "rectangular_symmetric", "drop_identity": False},
+    }
+
+    def compile(self, seq, registers):
+        print("hello")
+        seq = GBSSpecs().compile(seq, registers)
+        A, B, C = group_operations(seq, lambda x: isinstance(x, ops.MeasureFock))
+
+        if len(B[0].reg) != self.modes:
+            raise CircuitError("All modes must be measured.")
+
+        tmp_seq = seq[:-1] #This must be the measurements
+        meas_seq = seq[-1]
+        seq = GausianUnitary().compile(tmp_seq, registers) + meas_seq
+
+        S = seq[0].op.p[0]
+        n_modes = len(S)//2
+        half_n_modes = n_modes//2
+        hbar = 2
+        cov = (hbar/2) * S @ S.T
+        A = Amat(cov, hbar=hbar)
+        B = A[:nmodes,:nmodes]
+        B00 = B[:nwg,:nwg]
+        B01 = B[:nwg,nwg:]
+        B10 = B[nwg:,:nwg]
+        B11 = B[nwg:,nwg:]
+        if not np.allclose(B00,0) or not np.allclose(B11,0):
+            raise ValueError('The Gaussian state being prepared does not correspond to a bipartite graph')
+        if not np.allclose(B01, B10):
+            raise ValueError('The Gaussian state being prepared does not correspond to a symmetric bipartite graph')
+        sqs, V = takagi(B01)
+        sqs = np.arctanh(sqs)
+        atol = 1e-3
+        for s in sqs:
+            if not np.allclose(s, 0, atol=atol, rtol=0) or not np.allclose(s, sq_amplitude, atol=atol, rtol=0):
+                raise ValueError('The squeezing parameters necessary for state preparation are outside the range')
+
+        # logic to convert back to SF commands that match the chip
+        sq_seq = [Command(ops.S2gate(sqs[i]), [registers[i], registers[i+half_n_modes]]) for i in range(half_n_modes)]
+        unitary_seq = [Command(ops.Interferometer(U, mesh="rectangular_symmetric", drop_identity=True), registers[:half_n_modes]),Command(ops.Interferometer(U, mesh="rectangular_symmetric", drop_identity=True), registers[half_n_modes:])]
+        return sq_seq + unitary_seq + meas_seq
