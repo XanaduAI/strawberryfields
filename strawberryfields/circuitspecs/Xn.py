6f40a88fea72b1adb90616170882e3ba4679e37c
Nicolas Quesada <zeitus@gmail.com>
2020-04-08 22:14:25 -0400

    Makes the linter happy

--- strawberryfields/circuitspecs/Xn.py
+++ strawberryfields/circuitspecs/Xn.py
@@ -1,12 +1,27 @@
+# Copyright 2019-2020 Xanadu Quantum Technologies Inc.
+
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+
+#     http://www.apache.org/licenses/LICENSE-2.0
+
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""Circuit class specification for the Xn class of circuits."""
+
+import numpy as np
 from thewalrus.quantum import Amat
 from thewalrus.symplectic import expand
 from strawberryfields.decompositions import takagi
-from .circuit_specs import CircuitSpecs
 from strawberryfields.program_utils import CircuitError, Command, group_operations
 import strawberryfields.ops as ops
+from .circuit_specs import CircuitSpecs
 from .gbs import GBSSpecs
 from .gaussian_unitary import GaussianUnitary
-import numpy as np
 
 
 class XnSpecs(CircuitSpecs):
@@ -30,16 +45,13 @@ class XnSpecs(CircuitSpecs):
     }
 
     decompositions = {
-        "BipartiteGraphEmbed": {
-            "mesh": "rectangular_symmetric",
-            "drop_identity": False,
-        },
+        "BipartiteGraphEmbed": {"mesh": "rectangular_symmetric", "drop_identity": False,},
     }
 
     def compile(self, seq, registers):
         n_modes = len(registers)
         seq = GBSSpecs().compile(seq, registers)
-        A, B, C = group_operations(seq, lambda x: isinstance(x, ops.MeasureFock))
+        A, B, _ = group_operations(seq, lambda x: isinstance(x, ops.MeasureFock))
 
         if len(B[0].reg) != self.modes:
             raise CircuitError("All modes must be measured.")
@@ -62,37 +74,20 @@ class XnSpecs(CircuitSpecs):
         B10 = B[half_n_modes:, :half_n_modes]
         B11 = B[half_n_modes:, half_n_modes:]
         if not np.allclose(B00, 0) or not np.allclose(B11, 0):
-            raise ValueError(
-                "The Gaussian state being prepared does not correspond to a bipartite graph"
-            )
+            raise ValueError("The Gaussian state being prepared does not correspond to a bipartite graph")
         if not np.allclose(B01, B10):
-            raise ValueError(
-                "The Gaussian state being prepared does not correspond to a symmetric bipartite graph"
-            )
+            raise ValueError("The Gaussian state being prepared does not correspond to a symmetric bipartite graph")
         sqs, U = takagi(B01)
         sqs = np.arctanh(sqs)
         atol = 1e-3
         for s in sqs:
-            if not np.allclose(s, 0, atol=atol, rtol=0) and not np.allclose(
-                s, self.sq_amplitude, atol=atol, rtol=0
-            ):
-                raise ValueError(
-                    "The squeezing parameters necessary for state preparation are outside the range"
-                )
+            if not np.allclose(s, 0, atol=atol, rtol=0) and not np.allclose(s, self.sq_amplitude, atol=atol, rtol=0):
+                raise ValueError("The squeezing parameters necessary for state preparation are outside the range")
 
         # logic to convert back to SF commands that match the chip
-        sq_seq = [
-            Command(ops.S2gate(sqs[i]), [registers[i], registers[i + half_n_modes]])
-            for i in range(half_n_modes)
-        ]
+        sq_seq = [Command(ops.S2gate(sqs[i]), [registers[i], registers[i + half_n_modes]]) for i in range(half_n_modes)]
         unitary_seq = [
-            Command(
-                ops.Interferometer(U, mesh="rectangular_symmetric", drop_identity=True),
-                registers[:half_n_modes],
-            ),
-            Command(
-                ops.Interferometer(U, mesh="rectangular_symmetric", drop_identity=True),
-                registers[half_n_modes:],
-            ),
+            Command(ops.Interferometer(U, mesh="rectangular_symmetric", drop_identity=True), registers[:half_n_modes],),
+            Command(ops.Interferometer(U, mesh="rectangular_symmetric", drop_identity=True), registers[half_n_modes:],),
         ]
         return sq_seq + unitary_seq + meas_seq
