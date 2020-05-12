29f3d5301a9df92131761d00fe55baf6157d3328
trbromley <brotho02@gmail.com>
2020-05-12 14:11:46 -0400

    Fix appearing in docs

--- strawberryfields/apps/train/__init__.py
+++ strawberryfields/apps/train/__init__.py
@@ -1,5 +1,10 @@
 r"""
 Tools for training variational GBS devices.
+
+.. autosummary::
+    :toctree: api
+
+    VGBS
 """
 from strawberryfields.apps.train.parametrization import VGBS
 import strawberryfields.apps.train.parametrization
