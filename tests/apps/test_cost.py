967f0875198b7b9b7917eaa044d9561029a845e9
ixfoduap <juanmiguel@xanadu.ai>
2020-05-21 10:07:58 -0400

    Adds first skeleton of tests

--- tests/apps/test_cost.py
+++ tests/apps/test_cost.py
@@ -17,5 +17,24 @@ Unit tests for strawberryfields.apps.train.embed
 # pylint: disable=no-self-use,unused-argument
 import numpy as np
 import pytest
-from strawberryfields.apps.train import cost
+from strawberryfields.apps.train import cost, param, embed
 
+
+test_data = [[0, 0, 0, 0], [1, 1, 1, 1], [1, 1, 1, 1], [0, 0, 2, 2], [0, 1, 2, 3], [1, 0, 0, 0]]
+test_data = np.array([[np.array(t[:d]) for t in td] for d in range(2, 5)])
+test_n_means = np.array([3/6, 3/6, 6/6, 7/6])
+n_mean = 1
+
+
+@pytest.mark.parametrize("i", range(3))
+class TestKL:
+    """Tests for the class ``train.cost.KL"""
+
+    def test_mean_data(self):
+        m = i + 2
+        A = np.eye(m)
+        params = np.zeros(m)
+        embedding = embed(params)
+        vgbs = param(A, n_mean, embedding, threshold=True)
+        kl = cost.KL(test_data[i], vgbs)
+        assert kl.mean_n_data() == test_n_means[:m]
