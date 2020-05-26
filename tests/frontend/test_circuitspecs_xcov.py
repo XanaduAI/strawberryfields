f6ffa823b6e6b5eaa7048833356754b7c7c5107d
Nicolas Quesada <zeitus@gmail.com>
2020-05-26 12:10:17 -0400

    Renames compilers

--- tests/frontend/test_circuitspecs_xcov.py
+++ tests/frontend/test_circuitspecs_xcov.py
@@ -11,8 +11,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-r"""Unit tests for the CircuitSpec class"""
-
+r"""Unit tests for the Xcov compiler"""
 import pytest
 import numpy as np
 import networkx as nx
