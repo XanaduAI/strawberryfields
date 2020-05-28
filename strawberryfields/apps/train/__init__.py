8f3cf68246fc50942d94f219a4e61f6fa2935225
ixfoduap <40441298+ixfoduap@users.noreply.github.com>
2020-05-28 18:07:47 -0400

    Add KL divergence to cost module (#391)
    * Saving first partial sketch
    
    * Simplifies expressions
    
    * First draft of class and methods
    
    * Adds first skeleton of tests
    
    * Adds example usage
    
    * Adds tests
    
    * black
    
    * Add to init
    
    * Update strawberryfields/apps/train/cost.py
    
    Co-authored-by: Tom Bromley <49409390+trbromley@users.noreply.github.com>
    
    * Update strawberryfields/apps/train/cost.py
    
    Co-authored-by: Tom Bromley <49409390+trbromley@users.noreply.github.com>
    
    * Update strawberryfields/apps/train/cost.py
    
    Co-authored-by: Tom Bromley <49409390+trbromley@users.noreply.github.com>
    
    * Update strawberryfields/apps/train/cost.py
    
    Co-authored-by: Tom Bromley <49409390+trbromley@users.noreply.github.com>
    
    * Apply suggestions from code review
    
    Co-authored-by: Tom Bromley <49409390+trbromley@users.noreply.github.com>
    
    * Changes from code review
    
    * Add suggestions from code review
    
    * Apply suggestions from code review
    
    Co-authored-by: Josh Izaac <josh146@gmail.com>
    
    * Apply suggestions from code review
    
    * Fix lint issues
    
    * Add pytest mark
    
    * Update init
    
    * Apply suggestions from code review
    
    Co-authored-by: Tom Bromley <49409390+trbromley@users.noreply.github.com>
    
    * Adds changes from code review
    
    * Fixes bug with data.shape
    
    * Adds test of gradient in threshold mode
    
    * Apply suggestions from code review
    
    Co-authored-by: Tom Bromley <49409390+trbromley@users.noreply.github.com>
    
    * Fixes trailing whitespace
    
    Co-authored-by: trbromley <brotho02@gmail.com>
    Co-authored-by: Tom Bromley <49409390+trbromley@users.noreply.github.com>
    Co-authored-by: Josh Izaac <josh146@gmail.com>

--- strawberryfields/apps/train/__init__.py
+++ strawberryfields/apps/train/__init__.py
@@ -9,7 +9,11 @@ Tools for training variational GBS devices.
     embed
     VGBS
     param
+    KL
+    cost
 """
 from strawberryfields.apps.train.param import VGBS
+from strawberryfields.apps.train.cost import KL
+import strawberryfields.apps.train.cost
 import strawberryfields.apps.train.param
 import strawberryfields.apps.train.embed
