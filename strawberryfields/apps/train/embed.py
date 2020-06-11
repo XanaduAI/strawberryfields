Fix training for KL divergence

strawberryfields/apps/train/embed.py
@@ -87,7 +87,7 @@ class ExpFeatures:
                 "Dimension of parameter vector must be equal to dimension of feature vectors"
             )
         w = self.weights(params)
-        return -self.features * w[:, np.newaxis]
+        return self.features * w[:, np.newaxis]
 
 
 class Exp(ExpFeatures):

strawberryfields/apps/train/param.py
@@ -227,7 +227,7 @@ class VGBS:
         Returns:
             array: the diagonal matrix of weights
         """
-        return np.sqrt(np.diag(self.embedding(params)))
+        return np.diag(self.embedding(params))
 
     def A(self, params: np.ndarray) -> np.ndarray:
         r"""Calculate the trained adjacency matrix :math:`A(\theta)`.