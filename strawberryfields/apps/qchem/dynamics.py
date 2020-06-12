0a47fb975a9167f34a3d99f79627cd1288022ce7
soranjh <soranjh@yahoo.com>
2020-06-12 13:58:27 -0400

    Add fock state sampling function and unittest

---
+++ strawberryfields/apps/qchem/dynamics.py
@@ -0,0 +1,197 @@
+# Copyright 2020 Xanadu Quantum Technologies Inc.
+
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+
+#     http://www.apache.org/licenses/LICENSE-2.0
+
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+r"""
+Functions used for simulating vibrational quantum dynamics of molecules.
+
+Photonic quantum devices can be programmed with molecular data in order to simulate the quantum
+dynamics of spatially-localized vibrations in molecules :cite:`sparrow2018simulating`. To that aim,
+the quantum device has to be programmed to implement the transformation:
+
+.. math::
+    U(t) = U_l e^{-i\hat{H}t/\hbar} U_l^\dagger,
+
+where :math:`\hat{H} = \sum_i \hbar \omega_i a_i^\dagger a_i` is the Hamiltonian corresponding to
+the harmonic normal modes, :math:`\omega_i` is the vibrational frequency of the :math:`i`-th normal
+mode, :math:`t` is time, and :math:`U_l` is a unitary matrix that relates the normal modes to a set
+of new modes that are localized on specific bonds or groups in a molecule. The matrix :math:`U_l`
+can be obtained by maximizing the sum of the squares of the atomic contributions to the modes
+:cite:`jacob2009localizing`. Having :math:`U_l` and :math:`\omega` for a given molecule, and assuming
+that it is possible to prepare the initial states of the mode, one can simulate the dynamics of
+vibrational excitations in the localized basis at any given time :math:`t`. This process has three
+main parts:
+
+- Preparation of an initial vibrational state.
+- Application of the dynamics transformation :math:`U(t)`.
+- Generating samples and computing the probability of observing desired states.
+
+It is noted that the initial states can be prepared in different ways. For instance, they can be
+Fock states or Gaussian states such as coherent states or two-mode squeezed vacuum states.
+
+Algorithm
+---------
+
+The algorithm for simulating the vibrational quantum dynamics in the localized basis with a photonic
+device has the following form:
+
+1. Each optical mode is assigned to a vibrational local mode and a specific initial excitation is
+   created using one of the state preparation methods discussed. A list of state preparations
+   methods available in Strawberry Fields is provided :doc:`here </introduction/ops>`.
+
+2. An interferometer is configured according to the unitary :math:`U_l^\dagger` and the initial
+   state is propagated through the interferometer.
+
+3. For each mode, a rotation gate is designed as :math:`R(\theta) = \exp(i\theta \hat{a}^{\dagger}\hat{a})`
+   where :math:`\theta = -\omega t`.
+
+4. A second interferometer is configured according to the unitary :math:`U_l` and the new state
+   is propagated through the interferometer.
+
+5. The number of photons in each output mode is measured.
+
+6. Samples are generated and the probability of obtaining a specific excitation in a given mode
+   (or modes) is computed for time :math:`t`.
+
+This module contains functions for implementing this algorithm. The function :func:`~.evolution`
+returns a custom ``sf`` operation that contains the required unitary and rotation operations
+explained in steps 2-4 of the algorithm.
+"""
+import numpy as np
+from scipy.constants import c, pi
+
+import strawberryfields as sf
+from strawberryfields.utils import operation
+
+
+def evolution(modes: int):
+    r"""Generates a custom ``sf`` operation for performing the transformation
+    :math:`U(t) = U_l e^{-i\hat{H}t/\hbar} U_l^\dagger` on a given state.
+
+    The custom operation returned by this function can be used as part of a Strawberry Fields
+    :class:`~.Program` just like any other operation from the :mod:`~.ops` module. Its arguments
+    are:
+
+    - t (float): time in femtoseconds
+    - Ul (array): normal to local transformation matrix :math:`U_l`
+    - w (array): normal mode frequencies :math:`\omega` in units of :math:`\mbox{cm}^{-1}` that
+      compose the Hamiltonian :math:`\hat{H} = \sum_i \hbar \omega_i a_i^\dagger a_i`
+
+    **Example usage:**
+
+    >>> modes = 2
+    >>> transform =  evolution(modes)
+    >>> p = sf.Program(modes)
+    >>> with p.context as q:
+    >>>     sf.ops.Fock(1) | q[0]
+    >>>     sf.ops.Fock(2) | q[1]
+    >>>     transform(t, Ul, w) | q
+
+    Args:
+        modes (int): number of modes
+
+    Returns:
+        an ``sf`` operation for enacting the dynamics transformation
+    Return type:
+        op
+    """
+    # pylint: disable=expression-not-assigned
+    @operation(modes)
+    def op(t, Ul, w, q):
+
+        theta = -w * 100.0 * c * 1.0e-15 * t * (2.0 * pi)
+
+        sf.ops.Interferometer(Ul.T) | q
+
+        for i in range(modes):
+            sf.ops.Rgate(theta[i]) | q[i]
+
+        sf.ops.Interferometer(Ul) | q
+
+    return op
+
+
+def sample_fock(
+    input_state: list,
+    t: float,
+    Ul: np.ndarray,
+    w: np.ndarray,
+    n_samples: int = 1,
+    loss: float = 0.0,
+    cutoff: int = 5,
+) -> list:
+
+    r"""Generate samples for simulating vibrational quantum dynamics with a Fock input state.
+
+    **Example usage:**
+    >>> input_state = [0, 2]
+    >>> t = 10.0
+    >>> Ul = np.array([[0.707106781, -0.707106781],
+    >>>                [0.707106781, 0.707106781]])
+    >>> w = np.array([3914.92, 3787.59])
+    >>> n_samples = 5
+    >>> sample_fock(input_state, t, Ul, w, n_samples = n_samples)
+    [[0, 2], [0, 2], [1, 1], [0, 2], [0, 2]]
+
+    Args:
+        input_state (list): input Fock state
+        t (float): time in femtoseconds
+        Ul (array): normal to local transformation matrix
+        w (array): normal mode frequencies :math:`\omega` in units of :math:`\mbox{cm}^{-1}`
+        n_samples (int): number of samples to be generated
+        loss (float): loss parameter denoting the fraction of generated photons that are lost
+        cutoff (int): cutoff dimension for determining the set of number states for each mode
+
+    Returns:
+        list[list[int]]: a list of samples
+    """
+    if n_samples < 1:
+        raise ValueError("Number of samples must be at least one")
+
+    if t < 0:
+        raise ValueError("Time must be zero or positive")
+
+    if np.any(w <= 0):
+        raise ValueError("Vibrational frequencies must be larger than zero")
+
+    if np.any(np.iscomplex(Ul)):
+        raise ValueError("The normal mode to local mode transformation matrix must be real")
+
+    if not 0 <= loss <= 1:
+        raise ValueError("Loss parameter must take a value between zero and one")
+
+    modes = len(Ul)
+    op = evolution(modes)
+    s = []
+
+    eng = sf.Engine("fock", backend_options={"cutoff_dim": cutoff})
+
+    gbs = sf.Program(modes)
+
+    # pylint: disable=expression-not-assigned
+    with gbs.context as q:
+
+        for i in range(modes):
+            sf.ops.Fock(input_state[i]) | q[i]
+
+        op(t, Ul, w) | q
+
+        if loss:
+            for _q in q:
+                sf.ops.LossChannel(1 - loss) | _q
+
+        sf.ops.MeasureFock() | q
+
+        for _ in range(n_samples):
+            s.append(eng.run(gbs).samples[0].tolist())
+
+    return s
---
+++ tests/apps/qchem/test_dynamics.py
@@ -0,0 +1,192 @@
+# Copyright 2020 Xanadu Quantum Technologies Inc.
+
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+
+#     http://www.apache.org/licenses/LICENSE-2.0
+
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+r"""
+Tests for strawberryfields.apps.qchem.dynamics
+"""
+import numpy as np
+import pytest
+
+import strawberryfields as sf
+from strawberryfields.apps.qchem import dynamics
+
+pytestmark = pytest.mark.apps
+
+
+t1 = 10.0
+t2 = 100.0
+a = 1.0 / np.sqrt(2.0)
+U1 = np.array([[a, -a], [a, a]])
+U2 = np.array(
+    [
+        [-0.06822734, -0.96080077, -0.26871343],
+        [-0.32019600, -0.23400449, 0.91799587],
+        [0.94489129, -0.14867339, 0.29167906],
+    ]
+)
+w1 = np.array([3914.92, 3787.59])
+w2 = np.array([3914.92, 3787.59, 1627.01])
+p1 = np.array(
+    [
+        [0.00000000e00, 0.00000000e00, 2.04851621e-04, 0.00000000e00],
+        [0.00000000e00, 2.82155738e-02, 0.00000000e00, 0.00000000e00],
+        [9.71579575e-01, 0.00000000e00, 0.00000000e00, 0.00000000e00],
+        [0.00000000e00, 0.00000000e00, 0.00000000e00, 0.00000000e00],
+    ]
+)
+p2 = np.array(
+    [
+        [
+            [0.00000000, 0.00000000, 0.00530293, 0.00000000],
+            [0.00000000, 0.03023791, 0.00000000, 0.00000000],
+            [0.04310503, 0.00000000, 0.00000000, 0.00000000],
+            [0.00000000, 0.00000000, 0.00000000, 0.00000000],
+        ],
+        [
+            [0.00000000, 0.10479863, 0.00000000, 0.00000000],
+            [0.29878705, 0.00000000, 0.00000000, 0.00000000],
+            [0.00000000, 0.00000000, 0.00000000, 0.00000000],
+            [0.00000000, 0.00000000, 0.00000000, 0.00000000],
+        ],
+        [
+            [0.51776846, 0.00000000, 0.00000000, 0.00000000],
+            [0.00000000, 0.00000000, 0.00000000, 0.00000000],
+            [0.00000000, 0.00000000, 0.00000000, 0.00000000],
+            [0.00000000, 0.00000000, 0.00000000, 0.00000000],
+        ],
+        [
+            [0.00000000, 0.00000000, 0.00000000, 0.00000000],
+            [0.00000000, 0.00000000, 0.00000000, 0.00000000],
+            [0.00000000, 0.00000000, 0.00000000, 0.00000000],
+            [0.00000000, 0.00000000, 0.00000000, 0.00000000],
+        ],
+    ]
+)
+
+
+@pytest.mark.parametrize("time, unitary, frequency, prob", [(t1, U1, w1, p1), (t2, U2, w2, p2)])
+def test_evolution(time, unitary, frequency, prob):
+    """Test if the function ``strawberryfields.apps.qchem.dynamics.evolution`` gives the correct
+    probabilities of all possible Fock basis states when used in a circuit"""
+
+    modes = len(unitary)
+    op = dynamics.evolution(modes)
+
+    eng = sf.Engine("fock", backend_options={"cutoff_dim": 4})
+    gbs = sf.Program(modes)
+
+    with gbs.context as q:
+
+        sf.ops.Fock(2) | q[0]
+
+        op(time, unitary, frequency) | q
+
+        p = eng.run(gbs).state.all_fock_probs()
+
+    assert np.allclose(p, prob)
+
+
+is1 = [0, 2]
+is2 = [0, 2, 0]
+ns1 = 1
+ns2 = 2
+p1 = [is1, t1, U1, w1, ns1]
+p2 = [is2, t2, U2, w2, ns2]
+
+
+@pytest.mark.parametrize("p", [p1, p2])
+class TestSampleFock:
+    """Tests for the function ``strawberryfields.apps.qchem.dynamics.sample_fock``"""
+
+    def test_invalid_n_samples(self, p):
+        """Test if function raises a ``ValueError`` when a number of samples less than one is
+        requested."""
+        with pytest.raises(ValueError, match="Number of samples must be at least one"):
+            in_state, t, U, w, ns = p
+            dynamics.sample_fock(in_state, t, U, w, 0)
+
+    def test_invalid_time(self, p):
+        """Test if function raises a ``ValueError`` when a negative time is given."""
+        with pytest.raises(ValueError, match="Time must be zero or positive"):
+            in_state, t, U, w, ns = p
+            dynamics.sample_fock(in_state, -t, U, w, ns)
+
+    def test_negative_frequency(self, p):
+        """Test if function raises a ``ValueError`` when negative frequencies are given."""
+        with pytest.raises(ValueError, match="Vibrational frequencies must be larger than zero"):
+            in_state, t, U, w, ns = p
+            dynamics.sample_fock(in_state, t, U, -w, ns)
+
+    def test_zero_frequency(self, p):
+        """Test if function raises a ``ValueError`` when zero frequencies are given."""
+        with pytest.raises(ValueError, match="Vibrational frequencies must be larger than zero"):
+            in_state, t, U, w, ns = p
+            dynamics.sample_fock(in_state, t, U, 0.0 * w, ns)
+
+    def test_complex_unitary(self, p):
+        """Test if function raises a ``ValueError`` when a complex unitary is given."""
+        with pytest.raises(
+            ValueError, match="The normal mode to local mode transformation matrix must be real"
+        ):
+            in_state, t, U, w, ns = p
+            dynamics.sample_fock(in_state, t, 1.0j * U, w, ns)
+
+    def test_invalid_loss(self, p):
+        """Test if function raises a ``ValueError`` when the loss parameter is specified outside
+        of range."""
+        with pytest.raises(
+            ValueError, match="Loss parameter must take a value between zero and one"
+        ):
+            in_state, t, U, w, ns = p
+            dynamics.sample_fock(in_state, t, U, w, ns, w[-1])
+
+    def test_loss(self, monkeypatch, p):
+        """Test if function correctly creates the SF program for lossy circuits."""
+
+        def save_hist(*args):
+            call_history.append(args[1])
+            return sf.engine.Result
+
+        call_history = []
+        with monkeypatch.context() as m:
+            m.setattr(sf.engine.Result, "samples", np.array([[0]]))
+            m.setattr(sf.LocalEngine, "run", save_hist)
+            dynamics.sample_fock(*p, loss=0.5)
+
+    def test_no_loss(self, monkeypatch, p):
+        """Test if function correctly creates the SF program for circuits without loss."""
+
+        def save_hist(*args):
+            call_history.append(args[1])
+            return sf.engine.Result
+
+        call_history = []
+        with monkeypatch.context() as m:
+            m.setattr(sf.engine.Result, "samples", np.array([[0]]))
+            m.setattr(sf.LocalEngine, "run", save_hist)
+            dynamics.sample_fock(*p)
+
+
+@pytest.mark.parametrize("p", [p1, p2])
+def test_fock_integration(p):
+    """Integration test for the function ``strawberryfields.apps.qchem.dynamics.sample_fock`` to
+    check if it returns samples of correct form, i.e., correct number of samples, correct number of
+    modes, all non-negative integers."""
+    samples = np.array(dynamics.sample_fock(*p))
+
+    dims = samples.shape
+
+    assert len(dims) == 2
+    assert dims == (p[4], len(p[2]))
+    assert samples.dtype == "int"
+    assert (samples >= 0).all()
