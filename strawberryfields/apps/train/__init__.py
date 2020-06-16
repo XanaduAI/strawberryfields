bf4acd5e78429939d493b338a680e17c05f8f87b
trbromley <brotho02@gmail.com>
2020-06-16 16:53:11 -0400

    Add vgbs module details

--- strawberryfields/apps/train/__init__.py
+++ strawberryfields/apps/train/__init__.py
@@ -20,7 +20,7 @@ of the weights with respect to the trainable parameters. There are two main clas
 corresponding to a different embedding. The :class:`Exp` class is a simple embedding where the
 weights are exponentials of the trainable parameters. The :class:`ExpFeatures` class is a more
 general embedding that makes use of user-defined feature vectors, which potentially provide more
-flexibility in training strategies."
+flexibility in training strategies.
 
 .. autosummary::
     :toctree: api
@@ -28,12 +28,44 @@ flexibility in training strategies."
     Exp
     ExpFeatures
 
+As we have discussed, the above embeddings output weights that are used within the :math:`W`
+matrix of the :math:`WAW` parametrization of GBS. This model of variational GBS is made available
+as the :class:`VGBS` class.
+
+.. autosummary::
+    :toctree: api
+
+    VGBS
+
+For example, a four-mode variational model can be constructed using:
+
+>>> from strawberryfields.apps import data
+>>> data = data.Mutag0()
+>>> embedding = Exp(data.modes)
+>>> n_mean = 5
+>>> vgbs = VGBS(data.adj, 5, embedding, threshold=False, samples=np.array(data[:1000]))
+
+We can then evaluate properties of the variational GBS distribution for different choices of
+trainable parameters:
+
+>>> params = 0.1 * np.ones(data.modes)
+>>> vgbs.n_mean(params)
+333
+
+Additional functionality related to the variational GBS model is available.
+
+.. autosummary::
+    :toctree: api
+
+    A_to_cov
+    prob_click
+    prob_photon_sample
+    rescale_adjacency
+
 .. autosummary::
     :toctree: api
 
     Stochastic
-    VGBS
-    param
     KL
     cost
 """
