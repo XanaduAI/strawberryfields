a38757dd5d857c2971011553332c602fb26a86f2
antalszava <antalszava@gmail.com>
2020-06-22 19:19:30 -0400

    Update strawberryfields/utils/post_processing.py
    Co-authored-by: Josh Izaac <josh146@gmail.com>

--- strawberryfields/utils/post_processing.py
+++ strawberryfields/utils/post_processing.py
@@ -51,10 +51,8 @@ def _check_modes(samples, modes):
     and the validity of the modes specified.
 
     Args:
-        samples (ndarray): the photon number samples with a shape
-            of (shots, modes)
-        modes (Sequence): the input modes to get the expectation value for, a
-            flattened sequence
+        samples (array): the photon number samples with a shape of ``(shots, modes)``
+        modes (Sequence): the input modes to get the expectation value for
     """
     num_modes = samples.shape[1]
     flattened_sequence_indices_msg = (
