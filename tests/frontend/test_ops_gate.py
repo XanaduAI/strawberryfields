0ccaa9edfc277cc48042a1c6ae924906ba1b1d42
antalszava <antalszava@gmail.com>
2020-07-08 20:20:44 -0400

    Move parametrize

--- tests/frontend/test_ops_gate.py
+++ tests/frontend/test_ops_gate.py
@@ -164,17 +164,16 @@ class TestGateBasics:
         # dagger should negate the first param
         assert applied_params == [-orig_params[0]] + orig_params[1:]
 
+@pytest.mark.parametrize("gate", [ops.Dgate, ops.Coherent, ops.DisplacedSqueezed])
 class TestComplexError:
     """Tests for raising an error if a parameter passed is complex"""
 
-    @pytest.mark.parametrize("gate", [ops.Dgate, ops.Coherent, ops.DisplacedSqueezed])
     def test_complex_first_argument_error(self, gate):
         """Test that passing a complex parameter to gates that previously accepted
         complex parameters raises an error."""
         with pytest.raises(ValueError, match="cannot be complex"):
             prog = Program(1)
             with prog.context as q:
-                print(gate)
                 gate(0.2+1j) | q
 
 
