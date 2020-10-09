7f7092edf87cb787372928ffd486f78dc07681d6
soranjh <soranjh@yahoo.com>
2020-10-08 22:07:42 -0400

    Add additional unittests

--- tests/apps/test_data.py
+++ tests/apps/test_data.py
@@ -362,6 +362,11 @@ class TestWaterDatasets:
         """Test if sample has correct shape"""
         assert np.shape(dataset[:]) == (5000, dataset.modes)
 
+    def test_times_water(self, dataset):
+        """Test if available times are correct"""
+        t = dataset.available_times()
+        assert np.allclose(t, np.linspace(0, 270, 28))
+
     def test_dataset_water(self, datasets):
         """Test if function raises a ``ValueError`` when an incorrect temperature is given"""
         with pytest.raises(ValueError, match="The selected time is not correct"):
@@ -425,6 +430,11 @@ class TestPyrroleDatasets:
     def test_samplep_dims(self, dataset):
         """Test if sample has correct shape"""
         assert np.shape(dataset[:]) == (1000, dataset.modes)
+
+    def test_times_pyrrole(self, dataset):
+        """Test if available times are correct"""
+        t = dataset.available_times()
+        assert np.allclose(t, np.linspace(0, 1000, 11))
 
     def test_dataset_pyrrole(self, datasets):
         """Test if function raises a ``ValueError`` when an incorrect temperature is given"""
