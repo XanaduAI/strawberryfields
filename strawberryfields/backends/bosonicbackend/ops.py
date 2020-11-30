294333bf391159af5d5bbf43a0ac22872057fca5
elib20 <53090166+elib20@users.noreply.github.com>
2020-11-30 11:24:40 -0500

    New functions for updating lists of means and covs
    Adds the functions chop_in_blocks_multi, chop_in_blocks_vector_multi, reassemble_multi, reassemble_vector_multi, which do the same thing as the non-multi version of the functions, but for arrays of means and covs instead of single mean and cov.

--- strawberryfields/backends/bosonicbackend/ops.py
+++ strawberryfields/backends/bosonicbackend/ops.py
@@ -32,6 +32,19 @@ def chop_in_blocks(m, idtodelete):
             C[localindex, localindex1] = m[globalindex, globalindex1]
     return (A, B, C)
 
+def chop_in_blocks_multi(m, idtodelete):
+    """
+    Splits an array of (symmetric) matrices each into 3 blocks, A, B, C
+    Blocks A and C are diagonal blocks and B is the offdiagonal block
+    idtodelete specifies which indices go into C.
+    """
+    A = np.copy(m)
+    A = np.delete(A, idtodelete, axis=1)
+    A = np.delete(A, idtodelete, axis=2)
+    B = np.delete(m[:,:, idtodelete], idtodelete, axis=1)
+    C = m[:,idtodelete,:][:,:,idtodelete]
+    return (A, B, C)
+
 
 def chop_in_blocks_vector(v, idtodelete):
     """
@@ -41,6 +54,16 @@ def chop_in_blocks_vector(v, idtodelete):
     idtokeep = list(set(np.arange(len(v))) - set(idtodelete))
     va = v[idtokeep]
     vb = v[idtodelete]
+    return (va, vb)
+
+def chop_in_blocks_vector_multi(v, idtodelete):
+    """
+    Splits a vector into two vectors, where idtodelete specifies
+    which elements go into vb
+    """
+    idtokeep = list(set(np.arange(len(v[0]))) - set(idtodelete))
+    va = v[:,idtokeep]
+    vb = v[:,idtodelete]
     return (va, vb)
 
 
@@ -61,6 +84,19 @@ def reassemble(A, idtodelete):
         newmat[i, i] = 1.0
     return newmat
 
+def reassemble_multi(A, idtodelete):
+    """
+    Puts the matrices A inside larger matrices of dimensions
+    dim(A)+len(idtodelete)
+    The empty space are filled with zeros (offdiagonal) and ones (diagonals)
+    """
+    nweights = len(A[:,0,0])
+    ntot = len(A[0]) + len(idtodelete)
+    ind = list(set(np.arange(ntot)) - set(idtodelete))
+    newmat = np.tile(np.eye(ntot,dtype=complex),(nweights,1,1))
+    newmat[np.ix_(np.arange(newmat.shape[0],dtype=int),ind,ind)] = A
+    return newmat
+
 
 def reassemble_vector(va, idtodelete):
     r"""Creates a vector with zeros indices idtodelete
@@ -71,4 +107,15 @@ def reassemble_vector(va, idtodelete):
     newv = np.zeros(ntot)
     for j, j1 in enumerate(ind):
         newv[j1] = va[j]
+    return newv
+
+def reassemble_vector_multi(va, idtodelete):
+    r"""Creates a vector with zeros indices idtodelete
+    and everywhere else it puts the entries of va
+    """
+    nweights = len(va[:,0])
+    ntot = len(va[0]) + len(idtodelete)
+    ind = list(set(np.arange(ntot)) - set(idtodelete))
+    newv = np.zeros((nweights,ntot),dtype=complex)
+    newv[:,ind] = va
     return newv
