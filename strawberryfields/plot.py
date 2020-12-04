67c1eba1e91fd208392a54f5f2af25aea8903b1b
Theodor Isacsson <theodor.isacsson@gmail.com>
2020-12-04 11:30:30 -0500

    Tidy up wigner plotting

--- strawberryfields/plot.py
+++ strawberryfields/plot.py
@@ -15,36 +15,33 @@ r"""
 This module provides tools to visualize the state in various interactive
 ways using Plot.ly
 """
-from copy import deepcopy
-
 import numpy as np
 
 plotly_error = (
     "Plot.ly required for using this function. It can be installed using pip install "
     "plotly or visiting https://plot.ly/python/getting-started/#installation"
 )
+def _get_plotly():
+    """Import Plot.ly on demand to avoid errors being raised unnecessarily"""
+    try:
+        # pylint:disable=import-outside-toplevel
+        import plotly.io as pio
+    except ImportError as e:
+        raise(plotly_error) from e
+    return pio
 
-try:
-    import plotly.io as pio
-except ImportError as e:
-    raise(plotly_error) from e
 
-
-def plot_wigner(state, p_axis, q_axis, renderer="browser", wire=0, dq=0.1):
+def plot_wigner(state, xvec, pvec, renderer="browser", wire=0):
     """Plot the Wigner function with Plot.ly
 
     Args:
         state (:class:`.BaseState`): the state used for plotting
-        p_axis (int): Range of the discretized :math:`p` quadrature values.
-            E.g., ranging from ``-p_axis`` to ``p_axis``.
-        q_axis (int): Range of the discretized :math:`x` quadrature values.
-            E.g., ranging from ``-q_axis`` to ``q_axis``.
+        xvec (array): array of discretized :math:`x` quadrature values
+        pvec (array): array of discretized :math:`p` quadrature values
         renderer (string): the renderer for plotting with Plot.ly
         wire (int): wire used to calculate the reduced Wigner function
-        dq (float): stepsize along the p_axis and q_axis
     """
-    xvec = np.arange(-q_axis, q_axis, dq)
-    pvec = np.arange(-p_axis, p_axis, dq)
+    pio = _get_plotly()
     pio.renderers.default = renderer
 
     data = state.wigner(wire, xvec, pvec)
@@ -64,30 +61,48 @@ def generate_wigner_chart(data, xvec, pvec):
     Returns:
         dict: a Plot.ly JSON-format surface plot
     """
+    textcolor = "#787878"
+
     chart = {
-        "data": [{"type": "scatter"}],
+        "data": [{
+            "type": "surface",
+            "colorscale": [],
+            "x": [],
+            "y": [],
+            "z": [],
+            "contours": {
+                "z": {},
+            },
+        }],
         "layout": {
             "scene":{
                 "xaxis": {},
                 "yaxis": {},
-                "zaxis": {}
+                "zaxis": {},
             }
         }
     }
-    chart["data"] = deepcopy(surfaceplotDefault["data"])
+
+    chart["data"][0]["type"] = "surface"
+    chart["data"][0]["colorscale"] = [
+        [0.0, "rgb(031, 144, 148)"],
+        [0.5, "rgb(255, 255, 255)"],
+        [1.0, "rgb(031, 089, 154)"],
+    ]
 
     chart["data"][0]["x"] = xvec.tolist()
     chart["data"][0]["y"] = pvec.tolist()
     chart["data"][0]["z"] = data.tolist()
+
+    chart["data"][0]["contours"]["z"]["show"] = True
 
     chart["data"][0]["cmin"] = -1 / np.pi
     chart["data"][0]["cmax"] = 1 / np.pi
 
     chart["layout"]["paper_bgcolor"] = "white"
     chart["layout"]["plot_bgcolor"] = "white"
+    chart["layout"]["font"] = {"color": textcolor}
     chart["layout"]["scene"]["bgcolor"] = "white"
-    chart["layout"]["font"] = {"color": textcolor}
-    chart["layout"]["font"] = {"color": textcolor}
 
     chart["layout"]["scene"]["xaxis"]["title"] = "x"
     chart["layout"]["scene"]["xaxis"]["color"] = textcolor
@@ -97,56 +112,3 @@ def generate_wigner_chart(data, xvec, pvec):
     chart["layout"]["scene"]["zaxis"]["title"] = "W(x,p)"
 
     return chart
-
-textcolor = "#787878"
-
-# Plot.ly default surface plot JSON
-surfaceplotDefault = {
-    "data": [{
-        "cmin": -1 / np.pi,
-        "cmax": 1 / np.pi,
-        "contours": {"z": {"show": True}},
-        "type": "surface",
-        "x": [],
-        "y": [],
-        "z": [],
-        "colorscale": [
-            [0.0, "rgb(31,144,148)"],
-            [0.5, "rgb(255,255,255)"],
-            [1.0, "rgb(31,89,154)"]
-        ],
-    }],
-    "layout": {
-        "width": 835,
-        "height": 500,
-        "margin": {
-            "l": 5,
-            "r": 5,
-            "b": 5,
-            "t": 5,
-            "pad": 4
-        },
-        "paper_bgcolor": "transparent",
-        "plot_bgcolor": "transparent",
-        "scene": {
-            "bgcolor": "transparent",
-            "xaxis": {
-                "gridcolor": textcolor,
-                "gridwidth": 2,
-            },
-            "yaxis": {
-                "gridcolor": textcolor,
-                "gridwidth": 2,
-            },
-            "zaxis": {
-                "autorange": True,
-                "gridcolor": textcolor,
-                "gridwidth": 2,
-            }
-        }
-    },
-    "config": {
-        "modeBarButtonsToRemove": ["zoom2d", "lasso2d", "select2d", "toggleSpikelines"],
-        "displaylogo": False
-    }
-}
