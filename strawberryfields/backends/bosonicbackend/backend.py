29d5d39f358592b2d6adc7fea2e52d6e541b2f21
elib20 <53090166+elib20@users.noreply.github.com>
2021-01-20 10:28:32 -0500

    minor mod
    Small change to match gkps branch

--- strawberryfields/backends/bosonicbackend/backend.py
+++ strawberryfields/backends/bosonicbackend/backend.py
@@ -262,8 +262,7 @@ class BosonicBackend(BaseBosonic):
 
     def begin_circuit(self, num_subsystems, **kwargs):
         self._init_modes = num_subsystems
-        self.circuit = BosonicModes()
-        self.circuit.reset(num_subsystems, 1)
+        self.circuit = BosonicModes(num_subsystems)
 
     def add_mode(self, n=1):
         self.circuit.add_mode([n])
