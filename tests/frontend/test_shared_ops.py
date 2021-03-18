905e3ffd4ed3c77dc1dc1b15e1ce26582ce76cfe
Nicolas Quesada <zeitus@gmail.com>
2021-03-18 10:49:51 -0400

    removes unused import

--- tests/frontend/test_shared_ops.py
+++ tests/frontend/test_shared_ops.py
@@ -12,7 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 r"""Unit tests for the Strawberry Fields shared_ops module"""
-import os
 import pytest
 
 pytestmark = pytest.mark.frontend
