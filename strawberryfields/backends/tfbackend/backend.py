81142d1107d16d799d7526492af9a58e963ca64e
Aaron Robertson <aaronrobertson.dev@gmail.com>
2021-03-20 09:07:46 -0700

    dtype as kwarg start

--- strawberryfields/backends/tfbackend/backend.py
+++ strawberryfields/backends/tfbackend/backend.py
@@ -83,6 +83,7 @@ class TFBackend(BaseFock):
                 For each mode, the simulator can represent the Fock states :math:`\ket{0}, \ket{1}, \ldots, \ket{\text{cutoff_dim}-1}`.
             pure (bool): If True (default), use a pure state representation (otherwise will use a mixed state representation).
             batch_size (None or int): Size of the batch-axis dimension. If None, no batch-axis will be used.
+            dtype (DType): Tensorflow element types in a tensor. 
         """
         cutoff_dim = kwargs.get("cutoff_dim", None)
         pure = kwargs.get("pure", True)
@@ -99,7 +100,7 @@ class TFBackend(BaseFock):
         if not isinstance(pure, bool):
             raise ValueError("Argument 'pure' must be either True or False")
         if not isinstance(dtype, DType):
-            raise ValueError("Argument 'dtype' must be a positive integer")
+            raise ValueError("Argument 'dtype' must be a Tensorflow DType")
         if batch_size == 1:
             raise ValueError(
                 "batch_size of 1 not supported, please use different batch_size or set batch_size=None"
@@ -107,7 +108,7 @@ class TFBackend(BaseFock):
 
         with tf.name_scope("Begin_circuit"):
             self._modemap = ModeMap(num_subsystems)
-            circuit = Circuit(num_subsystems, cutoff_dim, pure, batch_size)
+            circuit = Circuit(num_subsystems, cutoff_dim, pure, batch_size, dtype)
 
         self._init_modes = num_subsystems
         self.circuit = circuit
--- strawberryfields/backends/tfbackend/circuit.py
+++ strawberryfields/backends/tfbackend/circuit.py
@@ -37,7 +37,7 @@ from string import ascii_lowercase as indices
 import numpy as np
 from scipy.special import factorial
 import tensorflow as tf
-
+from tensorflow import DType
 # With TF 2.1+, the legacy tf.einsum was renamed to _einsum_v1, while
 # the replacement tf.einsum introduced the bug. This try-except block
 # will dynamically patch TensorFlow versions where _einsum_v1 exists, to make it the
@@ -62,10 +62,10 @@ class Circuit:
     The state of the modes is manipulated by calling the various methods."""
 
     # pylint: disable=too-many-instance-attributes,too-many-public-methods
-    def __init__(self, num_modes, cutoff_dim, pure, batch_size):
+    def __init__(self, num_modes, cutoff_dim, pure, batch_size, dtype):
         self._hbar = 2
         self.reset(
-            num_subsystems=num_modes, pure=pure, cutoff_dim=cutoff_dim, batch_size=batch_size
+            num_subsystems=num_modes, pure=pure, cutoff_dim=cutoff_dim, batch_size=batch_size, dtype=dtype
         )
 
     def _make_vac_states(self, cutoff_dim):
@@ -222,6 +222,7 @@ class Circuit:
             pure (bool): if True, the reset circuit will represent its state as a pure state. If False, the representation will be mixed.
             cutoff_dim (int): new Fock space cutoff dimension to use.
             batch_size (None, int): None means no batching. An integer value >= 2 sets the batch size to use.
+            dtype (DType): Default is tf.complex64. 
         """
         if "pure" in kwargs:
             pure = kwargs["pure"]
@@ -248,6 +249,13 @@ class Circuit:
                     raise ValueError("Argument 'batch_size' must be either None or an integer > 1")
             self._batch_size = batch_size
             self._batched = batch_size is not None
+
+        if "dtype" in kwargs:
+            dtype = kwargs["dtype"]
+            if dtype is not None:
+                if not isinstance(dtype, DType):
+                    raise ValueError("Argument 'dtype' must be a Tensorflow DType")
+            self._dtype = dtype
 
         self._state_history = []
         self._cache = {}
