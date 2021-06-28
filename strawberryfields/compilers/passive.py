ee1719f4ad13ce2814a092dd0d8ad1b5d9414cf5
Jake Bulmer <jake.bulmer@bristol.ac.uk>
2021-06-28 14:02:08 +0100

    more functional gate application

--- strawberryfields/compilers/passive.py
+++ strawberryfields/compilers/passive.py
@@ -31,6 +31,7 @@ def _apply_one_mode_gate(G, T, i):
     """
 
     T[i] *= G
+    return T
 
 
 def _apply_two_mode_gate(G, T, i, j):
@@ -43,6 +44,7 @@ def _apply_two_mode_gate(G, T, i, j):
         j (int): index of second mode of gate
     """
     (T[i], T[j]) = (G[0, 0] * T[i] + G[0, 1] * T[j], G[1, 0] * T[i] + G[1, 1] * T[j])
+    return T 
 
 
 def _beam_splitter_passive(theta, phi):
@@ -159,16 +161,16 @@ class Passive(Compiler):
             modes = [modes_label.ind for modes_label in operations.reg]
             if name == "Rgate":
                 G = np.exp(1j * params[0])
-                _apply_one_mode_gate(G, T, dict_indices[modes[0]])
+                T = _apply_one_mode_gate(G, T, dict_indices[modes[0]])
             elif name == "LossChannel":
                 G = np.sqrt(params[0])
-                _apply_one_mode_gate(G, T, dict_indices[modes[0]])
+                T = _apply_one_mode_gate(G, T, dict_indices[modes[0]])
             elif name == "Interferometer":
                 U = params[0]
                 if U.shape == (1, 1):
-                    _apply_one_mode_gate(U, T, dict_indices[modes[0]])
+                    T = _apply_one_mode_gate(U, T, dict_indices[modes[0]])
                 elif U.shape == (2, 2):
-                    _apply_two_mode_gate(U, T, dict_indices[modes[0]], dict_indices[modes[1]])
+                    T = _apply_two_mode_gate(U, T, dict_indices[modes[0]], dict_indices[modes[1]])
                 else:
                     modes = [dict_indices[mode] for mode in modes]
                     U_expand = np.eye(nmodes, dtype=np.complex128)
@@ -177,9 +179,9 @@ class Passive(Compiler):
             elif name == "PassiveChannel":
                 T0 = params[0]
                 if T0.shape == (1, 1):
-                    _apply_one_mode_gate(T0, T, dict_indices[modes[0]])
+                    T = _apply_one_mode_gate(T0, T, dict_indices[modes[0]])
                 elif T0.shape == (2, 2):
-                    _apply_two_mode_gate(T0, T, dict_indices[modes[0]], dict_indices[modes[1]])
+                    T = _apply_two_mode_gate(T0, T, dict_indices[modes[0]], dict_indices[modes[1]])
                 else:
                     modes = [dict_indices[mode] for mode in modes]
                     T0_expand = np.eye(nmodes, dtype=np.complex128)
@@ -187,19 +189,19 @@ class Passive(Compiler):
                     T = T0_expand @ T
             elif name == "BSgate":
                 G = _beam_splitter_passive(params[0], params[1])
-                _apply_two_mode_gate(G, T, dict_indices[modes[0]], dict_indices[modes[1]])
+                T = _apply_two_mode_gate(G, T, dict_indices[modes[0]], dict_indices[modes[1]])
             elif name == "MZgate":
                 v = np.exp(1j * params[0])
                 u = np.exp(1j * params[1])
                 U = 0.5 * np.array([[u * (v - 1), 1j * (1 + v)], [1j * u * (1 + v), 1 - v]])
-                _apply_two_mode_gate(U, T, dict_indices[modes[0]], dict_indices[modes[1]])
+                T = _apply_two_mode_gate(U, T, dict_indices[modes[0]], dict_indices[modes[1]])
             elif name == "sMZgate":
                 exp_sigma = np.exp(1j * (params[0] + params[1]) / 2)
                 delta = (params[0] - params[1]) / 2
                 U = exp_sigma * np.array(
                     [[np.sin(delta), np.cos(delta)], [np.cos(delta), -np.sin(delta)]]
                 )
-                _apply_two_mode_gate(U, T, dict_indices[modes[0]], dict_indices[modes[1]])
+                T = _apply_two_mode_gate(U, T, dict_indices[modes[0]], dict_indices[modes[1]])
 
         ord_reg = [r for r in list(registers) if r.ind in used_modes]
         ord_reg = sorted(list(ord_reg), key=lambda x: x.ind)
