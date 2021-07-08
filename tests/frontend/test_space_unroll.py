530562c254599be14834b2243f294e9da0c9af73
Nicolas Quesada <zeitus@gmail.com>
2021-07-08 15:41:56 -0400

    Adds new test: test_cov() currently failing

--- tests/frontend/test_space_unroll.py
+++ tests/frontend/test_space_unroll.py
@@ -68,18 +68,6 @@ def generate_valid_r_sequence(delays, modes, random_func=np.random.rand):
     return angles
 
 
-def is_permutation_matrix(x):
-    """Checks whether a matrix is a permutation matrix"""
-    x = np.asanyarray(x)
-    return (
-        x.ndim == 2
-        and x.shape[0] == x.shape[1]
-        and (x.sum(axis=0) == 1).all()
-        and (x.sum(axis=1) == 1).all()
-        and ((x == 1) | (x == 0)).all()
-    )
-
-
 @pytest.mark.parametrize("delays", [np.random.randint(low=1, high=10, size=i) for i in range(2, 6)])
 @pytest.mark.parametrize("modes", [10, 50, 100])
 def test_lossless_no_mixing_no_rotation_U(delays, modes):
@@ -88,12 +76,11 @@ def test_lossless_no_mixing_no_rotation_U(delays, modes):
     angles = np.zeros([2 * len(delays), modes + sum(delays)])
     d = len(delays)
     n, N = get_mode_indices(delays)
-    prog = sf.TDMProgram([N], delays=delays)
-    vac_modes = sum(delays)
+    prog = sf.TDMProgram([N])
     with prog.context(*angles) as (p, q):
         for i in range(d):
-            Rgate(p[i + d]) | q[n[i] + vac_modes]
-            BSgate(p[i], np.pi / 2) | (q[n[i + 1] + vac_modes], q[n[i] + vac_modes])
+            Rgate(p[i + d]) | q[n[i]]
+            BSgate(p[i], np.pi / 2) | (q[n[i + 1]], q[n[i]])
     prog.space_unroll(1)  # Not sure what is this
     compiled = prog.compile(compiler="passive")
     passive_elem = compiled.circuit[0]
@@ -102,35 +89,77 @@ def test_lossless_no_mixing_no_rotation_U(delays, modes):
     assert np.allclose(U, np.identity(len(U)))
 
 
-@pytest.mark.parametrize("delays", [np.array([1,10])])#[np.random.randint(low=1, high=10, size=i) for i in range(2, 6)])
-@pytest.mark.parametrize("modes", [100])#[70, 80, 100])
+@pytest.mark.parametrize("delays", [np.random.randint(low=1, high=10, size=i) for i in range(2, 6)])
+@pytest.mark.parametrize("modes", [70, 80, 100])
 def test_no_entanglement_between_padding_and_computational_modes(delays, modes):
     """Test that the U matrix is the identity if there is no beamsplitter mixing and no rotations"""
     delays = list(delays)
     angles = np.concatenate([generate_valid_bs_sequence(delays,modes), generate_valid_r_sequence(delays,modes)])
-    print(angles.shape)
     d = len(delays)
     n, N = get_mode_indices(delays)
-    prog = sf.TDMProgram([N], delays=delays)
+    prog = sf.TDMProgram([N])
     vac_modes = sum(delays)
-    print("N=",N)
     with prog.context(*angles) as (p, q):
         for i in range(d):
-            print(i, i+d, n[i], vac_modes)
-            Rgate(p[i + d]) | q[n[i] + vac_modes]
-            BSgate(p[i], np.pi / 2) | (q[n[i + 1] + vac_modes], q[n[i] + vac_modes])
+            Rgate(p[i + d]) | q[n[i]]
+            BSgate(p[i], np.pi / 2) | (q[n[i + 1]], q[n[i]])
     prog.space_unroll(1)  # Not sure what is this
     compiled = prog.compile(compiler="passive")
     passive_elem = compiled.circuit[0]
     U = passive_elem.op.p[0]
-    print(U.shape, vac_modes + modes)
     # Check that it is indeed the identity
     U_AA = U[:vac_modes,:vac_modes]
     U_AB = U[vac_modes:,:vac_modes]
     U_BA = U[:vac_modes,vac_modes:]
     U_BB = U[vac_modes:,vac_modes:]
-    #assert np.allclose(U @ U.T.conj(), np.identity(len(U)))
+
     assert np.allclose(U_AA, np.identity(vac_modes))
     assert np.allclose(U_AB, 0)
     assert np.allclose(U_BA, 0)
     assert np.allclose(U_BB @ U_BB.T.conj(), np.identity(len(U_BB)))
+
+@pytest.mark.parametrize("delays", [np.random.randint(low=1, high=10, size=i) for i in range(2,3)])
+@pytest.mark.parametrize("modes", [20])
+def test_is_permutation_when_angle_pi_on_two(delays, modes):
+    """Checks that if all the beamsplitters are cross then the absolute value output matrix is a permutation matrix"""
+    delays = list(delays)
+    net = modes + sum(delays)
+    angles = np.concatenate([generate_valid_bs_sequence(delays,modes), generate_valid_r_sequence(delays,modes)])
+    angles[0] = np.pi / 2 * np.random.randint(2, size=net)
+    angles[1] = np.pi / 2 * np.random.randint(2, size=net)
+    angles[2] = np.pi / 2 * np.random.randint(2, size=net)
+    d = len(delays)
+    n, N = get_mode_indices(delays)
+    prog = sf.TDMProgram([N])
+    vac_modes = sum(delays)
+    with prog.context(*angles) as (p, q):
+        for i in range(d):
+            Rgate(p[i + d]) | q[n[i]]
+            BSgate(p[i], np.pi / 2) | (q[n[i + 1]], q[n[i]])
+    prog.space_unroll(1)  # Not sure what is this
+    compiled = prog.compile(compiler="passive")
+    passive_elem = compiled.circuit[0]
+    U = passive_elem.op.p[0]
+    assert np.allclose(U@U.T.conj(), np.identity(len(U)))
+    assert np.allclose(list(map(max,np.abs(U))), 1.0)
+
+
+
+def test_cov():
+    """Tests space unrolling when going into the Gaussian backend"""
+    delays = [1,6,36]
+    modes = 216
+    net = modes + sum(delays)
+    d = len(delays)
+    angles = np.concatenate([generate_valid_bs_sequence(delays,modes), generate_valid_r_sequence(delays,modes)])
+    n, N = get_mode_indices(delays)
+    prog = sf.TDMProgram([N])
+    vac_modes = sum(delays)
+    with prog.context(*angles) as (p, q):
+        Sgate(0.8)|q[n[0]]
+        for i in range(d):
+            Rgate(p[i + d]) | q[n[i]]
+            BSgate(p[i], np.pi / 2) | (q[n[i + 1]], q[n[i]])
+    prog.space_unroll(1)  # Not sure what is
+    eng = sf.Engine(backend="gaussian")
+    results = eng.run(prog)
