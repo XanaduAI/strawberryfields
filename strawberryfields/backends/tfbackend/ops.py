8eb486fc6404a4342917dea3358061ac2fde3951
sylviemonet <sylvieyao502@gmail.com>
2021-08-02 12:05:27 +0800

    fix

--- strawberryfields/backends/tfbackend/ops.py
+++ strawberryfields/backends/tfbackend/ops.py
@@ -466,12 +466,12 @@ def single_gaussian_gate_matrix(R, y, C, cutoff, dtype=tf.complex64.as_numpy_dty
     C = C.numpy()
     y = y.numpy()
     R = R.numpy()
-    gate = gaussian_gate_tw(R, cutoff, y, C = C, dtype)
+    gate = gaussian_gate_tw(R, cutoff, y, C = C, dtype = dtype)
 
     @tf.function
     def grad(dy):
         dG_dC, dG_dR, dG_dy = grad_gaussian_gate_tw(
-            gate, R, cutoff, y, C = C, dtype
+            gate, R, cutoff, y, C = C, dtype = dtype
         )
         grad_C = tf.math.real(tf.reduce_sum(dy * tf.math.conj(dG_dC)))
         grad_y = tf.math.real(tf.reduce_sum(dy * tf.math.conj(dG_dy)))
