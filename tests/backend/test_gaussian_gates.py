cd70893af7f99ed176c4205845872a6de32d1e96
sylviemonet <sylvieyao502@gmail.com>
2021-08-05 09:28:17 +0800

    test 2

--- tests/backend/test_gaussian_gates.py
+++ tests/backend/test_gaussian_gates.py
@@ -45,7 +45,7 @@ class TestUnitaryFunctionRelated:
         assert np.allclose(_Sigma, expected_Sigma, atol=tol, rtol=0)
     
     @pytest.mark.parametrize("cutoff", [5,7])
-    def test_n_mode_gate(cutoff, tol):
+    def test_n_mode_gate(self, cutoff, tol):
         """Test if n_mode_gate is compatable with single and two-mode gate in tfbackend"""
         #single mode gate
         _pure = True
