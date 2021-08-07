d5001aa14016e5a37f8b40b39a654721e0d1708d
sduquemesa <675763+sduquemesa@users.noreply.github.com>
2021-08-07 17:18:33 -0500

    implementation [backends/fockbackend] - mzgate

--- strawberryfields/backends/fockbackend/circuit.py
+++ strawberryfields/backends/fockbackend/circuit.py
@@ -221,14 +221,14 @@ class Circuit:
         """Applies a two-mode gate to the state.
 
         Applies the specified two-mode gate to the state using custom tensor contractions and
-        the Numba compiler for faster application. Currently, only the beamsplitter and the
-        two-mode squeeze gate are supported.
+        the Numba compiler for faster application. Currently, only the beamsplitter, the
+        Mach-Zehnder and two-mode squeeze gate are supported.
 
         Args:
             mat (array[complex]): The numeric operator to be applied to the state, of shape `[trunc]*(2*n)`
             modes (list[int]): The list of modes to which the operator is applied on
             gate (str): The gate that is being applied. This argument determines the selection rules that
-                are used. Options are ``"BSgate"`` and ``"S2gate"``.
+                are used. Options are ``"BSgate"``, ``"MZgate`` and ``"S2gate"``.
 
         Returns:
             array[complex]: The state after application of the two-mode operation
@@ -250,12 +250,14 @@ class Circuit:
 
             if gate == "BSgate":
                 self._state = self._apply_BS(mat, self._state, self._trunc)
+            elif gate == "MZgate":
+                self._state = self._apply_MZ(mat, self._state, self._trunc)
             elif gate == "S2gate":
                 self._state = self._apply_S2(mat, self._state, self._trunc)
             else:
                 raise NotImplementedError(
-                    "Currently, selection rules are only implemented for the BSgate "
-                    "and the S2gate. The {} gate is not supported".format(gate)
+                    "Currently, selection rules are only implemented for the BSgate, "
+                    "the MZgate and the S2gate. The {} gate is not supported".format(gate)
                 )
 
             self._state = self._state.transpose(switch_list_2)
@@ -289,6 +291,13 @@ class Circuit:
                 self._state = self._state.transpose(switch_list_2)
                 self._state = self._apply_BS(mat.conj(), self._state, self._trunc)
 
+            elif gate == "MZgate":
+                self._state = self._apply_MZ(mat, self._state, self._trunc)
+                self._state = self._state.transpose(switch_list_1)
+
+                self._state = self._state.transpose(switch_list_2)
+                self._state = self._apply_MZ(mat.conj(), self._state, self._trunc)
+
             elif gate == "S2gate":
                 self._state = self._apply_S2(mat, self._state, self._trunc)
                 self._state = self._state.transpose(switch_list_1)
@@ -315,6 +324,37 @@ class Circuit:
 
         .. math::
             B_{ij}^{kl} \propto \delta_{i+j}^{k+l}
+
+        This implies that one can contract pure states (without loss of generality
+        we assume only two modes) using one less loop as follows:
+
+        .. math::
+            c'_{i, j} = \sum\limits_{k = \text{max}(1+i+j-n, 0)}
+                    ^{\text{min}(i+j, n-1) + 1}
+            B_{ik}^{j, i+j-k} c_{k, i+j-k}
+
+        where :math:`c_{kl}` is the tensor representing the two mode state and
+        :math:`n` is the cutoff in Fock space. Similarly for a mixed state it is
+        possible to do the update of the state using two less summations.
+        """
+        ret = np.zeros_like(state, dtype=np.complex128)
+        for i in range(trunc):
+            for j in range(trunc):
+                for k in range(max(1 + i + j - trunc, 0), min(i + j, trunc - 1) + 1):
+                    ret[i, j] += mat[i, k, j, i + j - k] * state[k, i + j - k]
+        return ret
+
+    # ignored in covtest (doesn't work well with the jit decorator)
+    @staticmethod
+    @jit(nopython=True)
+    def _apply_MZ(mat, state, trunc):  # pragma: no cover
+        r"""Applies the MZ gate to the first bra in state.
+
+        The Mach-Zehnder matrix elements :math:`B_{ij}^{kl}` satisfy the selection
+        rules
+
+        .. math::
+            MZ_{ij}^{kl} \propto \delta_{i+j}^{k+l}
 
         This implies that one can contract pure states (without loss of generality
         we assume only two modes) using one less loop as follows:
@@ -554,6 +594,13 @@ class Circuit:
         """
         mat = ops.beamsplitter(theta, phi, self._trunc)
         self._state = self.apply_twomode_gate(mat, [mode1, mode2], gate="BSgate")
+
+    def mzgate(self, phi_in, phi_ex, mode1, mode2):
+        """
+        Applies a MZ gate.
+        """
+        mat = ops.mzgate(phi_in, phi_ex, self._trunc)
+        self._state = self.apply_twomode_gate(mat, [mode1, mode2], gate="MZgate")
 
     def squeeze(self, r, theta, mode):
         """
