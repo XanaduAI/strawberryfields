eb6d8a5ae44251d520e1886eef2225b23db26d8b
sylviemonet <sylvieyao502@gmail.com>
2021-08-22 14:33:04 +0800

    ahhhh

--- tests/backend/test_gaussian_gates.py
+++ tests/backend/test_gaussian_gates.py
@@ -144,7 +144,7 @@ class TestFockRepresentation:
         d = np.random.random(num_mode)
         backend = setup_backend(2)
         backend.prepare_vacuum_state([0, 1])
-        backend.gaussian_gate(S, d, 0, 1)
+        backend.gaussian_gate(S, d, [0, 1])
         s = backend.state().ket()
         X = np.zeros((cutoff, cutoff))
         X[0,0] = 1
