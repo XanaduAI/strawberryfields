8e4f0cc7369e1bc5871a87bc37d5563c23fbebbc
Theodor Isacsson <theodor@xanadu.ai>
2021-12-02 17:59:54 -0500

    suggestion from code review

--- tests/api/test_result.py
+++ tests/api/test_result.py
@@ -95,10 +95,10 @@ class TestResultPrint:
         """Test that printing a result object with no state provides the correct output."""
         result = Result({"output": [np.array([[1, 2], [3, 4], [5, 6]])]})
         print(result)
-        out, _ = capfd.readouterr()
-        assert "modes=2" in out
-        assert "shots=3" in out
-        assert "contains state=False" in out
+        captured = capfd.readouterr()
+        assert "modes=2" in captured.out
+        assert "shots=3" in captured.out
+        assert "contains state=False" in captured.out
 
     def test_state_print(self, capfd):
         """Test that printing a result object with a state provides the correct output."""
@@ -108,21 +108,21 @@ class TestResultPrint:
         result = Result(samples, samples_dict=samples_dict)
         result.state = BaseGaussianState((np.array([0.0, 0.0]), np.identity(2)), 1)
         print(result)
-        out, _ = capfd.readouterr()
-        assert "modes=2" in out
-        assert "shots=3" in out
-        assert "contains state=True" in out
+        captured = capfd.readouterr()
+        assert "modes=2" in captured.out
+        assert "shots=3" in captured.out
+        assert "contains state=True" in captured.out
 
     def test_tdm_print(self, capfd):
         """Test that printing a result object with TDM samples provides the correct output."""
         samples = np.ones((2, 3, 4))
         result = Result({"output": [samples]})
         print(result)
-        out, _ = capfd.readouterr()
-        assert "spatial_modes=3" in out
-        assert "shots=2" in out
-        assert "timebins=4" in out
-        assert "contains state=False" in out
+        captured = capfd.readouterr()
+        assert "spatial_modes=3" in captured.out
+        assert "shots=2" in captured.out
+        assert "timebins=4" in captured.out
+        assert "contains state=False" in captured.out
 
     def test_unknown_shape_print(self, capfd):
         """Test that printing a result object with samples with an unknown shape
@@ -130,8 +130,8 @@ class TestResultPrint:
         samples = np.ones((2, 3, 4, 5))
         result = Result({"output": [samples]})
         print(result)
-        out, _ = capfd.readouterr()
-        assert "modes" not in out
-        assert "shots" not in out
-        assert "timebins" not in out
-        assert "contains state=False" in out
+        captured = capfd.readouterr()
+        assert "modes" not in captured.out
+        assert "shots" not in captured.out
+        assert "timebins" not in captured.out
+        assert "contains state=False" in captured.out
