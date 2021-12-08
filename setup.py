4161918a3f4221b1722bea868cee6db689ff78bf
Theodor <theodor@xanadu.ai>
2021-12-08 11:52:01 -0500

    Update readme to markdown (#664)
    * update readme
    
    * remove non-existing image
    
    * fix
    
    * update include
    
    * add to changelog
    
    * fix
    
    * update setup
    
    * Update .github/CHANGELOG.md
    
    Co-authored-by: Josh Izaac <josh146@gmail.com>
    
    Co-authored-by: Josh Izaac <josh146@gmail.com>

--- setup.py
+++ setup.py
@@ -11,9 +11,6 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-import os
-import sys
-
 from setuptools import setup, find_packages
 
 
@@ -49,8 +46,8 @@ info = {
                                           "apps/data/sample_data/*"]},
     "include_package_data": True,
     "description": "Open source library for continuous-variable quantum computation",
-    "long_description": open("README.rst", encoding="utf-8").read(),
-    "long_description_content_type": "text/x-rst",
+    "long_description": open("README.md", encoding="utf-8").read(),
+    "long_description_content_type": "text/markdown",
     "provides": ["strawberryfields"],
     "install_requires": requirements,
     "command_options": {
